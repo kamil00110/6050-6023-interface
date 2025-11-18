95d5e792bdf3b73bde04e34b6ab2d41e3a9f6f75
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-11-18 01:59:29 +0100

    Update interfaceitem.cpp

--- client/src/network/interfaceitem.cpp
+++ client/src/network/interfaceitem.cpp
@@ -52,13 +52,20 @@ QString InterfaceItem::displayName() const
 QString InterfaceItem::helpText() const
 {
     QString id;
-    if(QVariant attr = getAttribute(AttributeName::Help, QVariant()); attr.isValid())
-        id = attr.toString();
-    else
-        return QString(); // or return id = QString(object().classId()).append(':').append(name()).append(".help");
-    return Locale::tr(id);
-}
 
+    if (QVariant attr = getAttribute(AttributeName::Help, QVariant()); attr.isValid())
+        id = attr.toString();
+    else if (QVariant displayAttr = getAttribute(AttributeName::DisplayName, QVariant()); displayAttr.isValid())
+        id = displayAttr.toString() + "/help";
+    else
+        id = QString(object().classId()) + ":" + name() + "/help";
+
+    QString translated = Locale::tr(id);
+    if (translated == id)
+        return QString(); 
+
+    return translated;
+}
 
 bool InterfaceItem::hasAttribute(AttributeName name) const
 {

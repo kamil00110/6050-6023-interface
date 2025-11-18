419e24e0983a21ada2fd387e698fe114b4f92c0a
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-11-18 20:55:41 +0100

    Update Marklin6050Interface.cpp

--- server/src/hardware/interface/Marklin6050Interface.cpp
+++ server/src/hardware/interface/Marklin6050Interface.cpp
@@ -19,7 +19,7 @@ Marklin6050Interface::Marklin6050Interface(World& world, std::string_view objId)
       s88interval(this, "s88interval", 0, PropertyFlags::ReadWrite | PropertyFlags::Store),
       extensions(this, "extensions", false, PropertyFlags::ReadWrite | PropertyFlags::Store),
       debug(this, "debug", 0, PropertyFlags::ReadWrite | PropertyFlags::Store),
-      programmer(this, "programmer", 0, PropertyFlags::ReadWrite | PropertyFlags::Store)
+      programmer(this, "programmer", false, PropertyFlags::ReadWrite | PropertyFlags::Store)
       
   
 {

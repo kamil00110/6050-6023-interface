2f16d39f05f4a0224eac1fa54648036901ad953b
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 21:58:33 +0100

    Update 3dSound.cpp

--- server/src/hardware/3dSound/3dSound.cpp
+++ server/src/hardware/3dSound/3dSound.cpp
@@ -1,51 +1,42 @@
 #include "3dSound.hpp"
-#include "list/3dSoundList.hpp"
-#include "../../world/world.hpp"
 #include "../../core/attributes.hpp"
 
-std::shared_ptr<Sound3D> Sound3D::create(World& world, std::string_view _id)
+3dSound::3dSound(World& world, std::string_view _id)
+    : IdObject(world, _id)
+    , name{this, "name", std::string{}, PropertyFlags::ReadWrite | PropertyFlags::Store}
+    , x{this, "x", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
+    , y{this, "y", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
+    , z{this, "z", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
+    , volume{this, "volume", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
+    , pitch{this, "pitch", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
 {
-    auto sound = std::make_shared<Sound3D>(world, _id);
-    sound->addToWorld();
-    return sound;
+    Attributes::addDisplayName(name, "3D Sound Name");
+    Attributes::addDisplayName(x, "Position X");
+    Attributes::addDisplayName(y, "Position Y");
+    Attributes::addDisplayName(z, "Position Z");
+    Attributes::addDisplayName(volume, "Volume");
+    Attributes::addDisplayName(pitch, "Pitch");
 }
 
-Sound3D::Sound3D(World& world, std::string_view _id)
-    : Object(world, _id)
-    , name(this, "name", id, PropertyFlags::ReadWrite | PropertyFlags::Store)
-    , x(this, "x", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
-    , y(this, "y", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
-    , z(this, "z", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
-    , volume(this, "volume", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
-    , pitch(this, "pitch", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
-    , looping(this, "looping", false, PropertyFlags::ReadWrite | PropertyFlags::Store)
-    , play(*this, "play", MethodFlags::ScriptCallable, [this]() { fireEvent("play"); })
-    , stop(*this, "stop", MethodFlags::ScriptCallable, [this]() { fireEvent("stop"); })
-    , pause(*this, "pause", MethodFlags::ScriptCallable, [this]() { fireEvent("pause"); })
-    , onEvent(*this, "on_event", EventFlags::Scriptable)
+void 3dSound::addToWorld()
 {
-    m_interfaceItems.add(name);
-    m_interfaceItems.add(x);
-    m_interfaceItems.add(y);
-    m_interfaceItems.add(z);
-    m_interfaceItems.add(volume);
-    m_interfaceItems.add(pitch);
-    m_interfaceItems.add(looping);
-    m_interfaceItems.add(play);
-    m_interfaceItems.add(stop);
-    m_interfaceItems.add(pause);
-    m_interfaceItems.add(onEvent);
+    IdObject::addToWorld();
+    // Here you could add to a 3D sound manager if you have one
 }
 
-void Sound3D::addToWorld()
+void 3dSound::loaded()
 {
-    m_world.get3DSounds()->addObject(shared_from_this());
+    IdObject::loaded();
 }
 
-void Sound3D::loaded() {}
-void Sound3D::destroying() {}
-
-void Sound3D::fireEvent(const std::string& type)
+void 3dSound::destroying()
 {
-    Object::fireEvent(onEvent, type);
+    IdObject::destroying();
 }
+
+void 3dSound::play()
+{
+    onPlay.fire();
+    // Could trigger your actual 3D sound playback here
+}
+

e498cb401816409224707c7524a3e4523b9f25f7
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 21:56:54 +0100

    Update 3dSoundList.hpp

--- server/src/hardware/3dSound/list/3dSoundList.hpp
+++ server/src/hardware/3dSound/list/3dSoundList.hpp
@@ -1,20 +1,43 @@
-#ifndef TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLIST_HPP
-#define TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLIST_HPP
-
-#include "../../../core/objectlist.hpp"
+#pragma once
 #include "../3dSound.hpp"
+#include "../../../core/objectlist.hpp"
+#include "../../../world/getworld.hpp"
+#include "../../../core/attributes.hpp"
 
 class 3dSoundList : public ObjectList<3dSound>
 {
 public:
-    3dSoundList(Object& parent, std::string_view parentPropertyName);
+    3dSoundList(Object& parent, std::string_view parentPropertyName)
+        : ObjectList<3dSound>(parent, parentPropertyName)
+    {
+        const bool editable = contains(getWorld(parent).state.value(), WorldState::Edit);
 
-    TableModelPtr getModel();
+        // Simple "create" method
+        create = Method<3dSoundList, std::shared_ptr<3dSound>>(
+            *this, "create", [this, &parent]() {
+                auto& world = getWorld(parent);
+                auto sound = std::make_shared<3dSound>(world, world.getUniqueId("3dSound"));
+                addObject(sound);
+                return sound;
+            });
 
-    void worldEvent(WorldState state, WorldEvent event) override;
+        Attributes::addDisplayName(create, "Create 3D Sound");
+        Attributes::addEnabled(create, editable);
+    }
 
-protected:
-    bool isListedProperty(std::string_view name) override;
+    void worldEvent(WorldState state, WorldEvent event)
+    {
+        ObjectList<3dSound>::worldEvent(state, event);
+        const bool editable = contains(state, WorldState::Edit);
+        Attributes::setEnabled(create, editable);
+    }
+
+    bool isListedProperty(std::string_view name) override
+    {
+        // You can list properties you care about for the ObjectList here
+        return name == "name"; // Example
+    }
+
+private:
+    Method<3dSoundList, std::shared_ptr<3dSound>> create;
 };
-
-#endif

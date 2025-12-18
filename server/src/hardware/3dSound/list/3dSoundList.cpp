b83695ee8c94cacc75523f3c71dfc45db8b44c78
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 22:17:23 +0100

    Update 3dSoundList.cpp

--- server/src/hardware/3dSound/list/3dSoundList.cpp
+++ server/src/hardware/3dSound/list/3dSoundList.cpp
@@ -1,12 +1,13 @@
 #include "3dSoundList.hpp"
-#include "../../world/getworld.hpp"
-#include "../../core/attributes.hpp"
+#include "../../../world/getworld.hpp"
+#include "../../../core/attributes.hpp"
 
 3dSoundList::3dSoundList(Object& parent, std::string_view parentPropertyName)
     : ObjectList<3dSound>(parent, parentPropertyName)
 {
     const bool editable = contains(getWorld(parent).state.value(), WorldState::Edit);
 
+    // Add "create" method
     create = Method<3dSoundList, std::shared_ptr<3dSound>>(
         *this, "create", [this]() {
             auto& world = getWorld(parent());
@@ -17,4 +18,17 @@
 
     Attributes::addDisplayName(create, "Create 3D Sound");
     Attributes::addEnabled(create, editable);
+}
+
+void 3dSoundList::worldEvent(WorldState state, WorldEvent event)
+{
+    ObjectList<3dSound>::worldEvent(state, event);
+    const bool editable = contains(state, WorldState::Edit);
+    Attributes::setEnabled(create, editable);
+}
+
+bool 3dSoundList::isListedProperty(std::string_view name)
+{
+    // For now we just list all properties of 3dSound
+    return name == "name"; // Example property, replace with actual if needed
 }

e696088d5aeac6e0ef6f741845405df6ed258354
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 22:00:02 +0100

    Update 3dSoundList.hpp

--- server/src/hardware/3dSound/list/3dSoundList.hpp
+++ server/src/hardware/3dSound/list/3dSoundList.hpp
@@ -1,43 +1,27 @@
 #pragma once
+#include "../../core/objectlist.hpp"
 #include "../3dSound.hpp"
-#include "../../../core/objectlist.hpp"
-#include "../../../world/getworld.hpp"
-#include "../../../core/attributes.hpp"
+#include <string>
 
 class 3dSoundList : public ObjectList<3dSound>
 {
 public:
-    3dSoundList(Object& parent, std::string_view parentPropertyName)
-        : ObjectList<3dSound>(parent, parentPropertyName)
-    {
-        const bool editable = contains(getWorld(parent).state.value(), WorldState::Edit);
+    Method<3dSoundList, std::shared_ptr<3dSound>> create;
 
-        // Simple "create" method
-        create = Method<3dSoundList, std::shared_ptr<3dSound>>(
-            *this, "create", [this, &parent]() {
-                auto& world = getWorld(parent);
-                auto sound = std::make_shared<3dSound>(world, world.getUniqueId("3dSound"));
-                addObject(sound);
-                return sound;
-            });
+    3dSoundList(Object& parent, std::string_view parentPropertyName);
 
-        Attributes::addDisplayName(create, "Create 3D Sound");
-        Attributes::addEnabled(create, editable);
-    }
+    TableModelPtr getModel() override { return nullptr; } // no table model for now
 
-    void worldEvent(WorldState state, WorldEvent event)
+    void worldEvent(WorldState state, WorldEvent event) override
     {
         ObjectList<3dSound>::worldEvent(state, event);
         const bool editable = contains(state, WorldState::Edit);
         Attributes::setEnabled(create, editable);
     }
 
-    bool isListedProperty(std::string_view name) override
+protected:
+    bool isListedProperty(std::string_view /*name*/) override
     {
-        // You can list properties you care about for the ObjectList here
-        return name == "name"; // Example
+        return false; // no properties listed for now
     }
-
-private:
-    Method<3dSoundList, std::shared_ptr<3dSound>> create;
 };

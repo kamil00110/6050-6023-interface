c3f7194468b192f868aa6f7b767010a3c81574d3
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 21:20:45 +0100

    Update 3dSoundList.cpp

--- server/src/hardware/3dSound/list/3dSoundList.cpp
+++ server/src/hardware/3dSound/list/3dSoundList.cpp
@@ -1,10 +1,35 @@
-/**
- * server/src/hardware/3dSound/list/3dSoundList.cpp
- *
- * Implements the list of 3D sounds.
- */
-
 #include "3dSoundList.hpp"
+#include "../../world/world.hpp"
+#include "../../core/attributes.hpp"
 
-// Currently no additional implementation required beyond ObjectList
+Sound3DList::Sound3DList(Object& _parent, std::string_view parentPropertyName)
+    : ObjectList<Sound3D>(_parent, parentPropertyName)
+    , create(*this, "create", [this]()
+    {
+        auto& world = dynamic_cast<World&>(parent());
+        return Sound3D::create(world, world.getUniqueId(Sound3D::defaultId));
+    })
+    , delete_(*this, "delete", [this](const ObjectPtr& obj)
+    {
+        removeObject(obj);
+        return true;
+    })
+{
+    m_interfaceItems.add(create);
+    m_interfaceItems.add(delete_);
+}
 
+TableModelPtr Sound3DList::getModel()
+{
+    return {}; // placeholder
+}
+
+void Sound3DList::worldEvent(WorldState state, WorldEvent event)
+{
+    ObjectList<Sound3D>::worldEvent(state, event);
+}
+
+bool Sound3DList::isListedProperty(std::string_view name)
+{
+    return false;
+}

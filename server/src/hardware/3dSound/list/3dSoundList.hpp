4eac90de6e821ac19dd4f05c98170fd2f2502484
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 20:56:38 +0100

    Update 3dSoundList.hpp

--- server/src/hardware/3dSound/list/3dSoundList.hpp
+++ server/src/hardware/3dSound/list/3dSoundList.hpp
@@ -1,1 +1,19 @@
+/**
+ * server/src/hardware/3dSound/list/3dSoundList.hpp
+ *
+ * Container/list class for all 3D sounds.
+ */
+
+#pragma once
+
+#include "../../core/objectlist.hpp"
+#include "../3dSound.hpp"
+
+class Sound3DList : public ObjectList<Sound3D>
+{
+public:
+  Sound3DList(World& world, std::string_view _name)
+    : ObjectList<Sound3D>(world, _name)
+  {}
+};
 

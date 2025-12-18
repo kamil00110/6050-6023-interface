dcd68fef1fd9096a60f1745389832dcaa6717dce
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 22:08:38 +0100

    Update 3dSoundList.hpp

--- server/src/hardware/3dSound/list/3dSoundList.hpp
+++ server/src/hardware/3dSound/list/3dSoundList.hpp
@@ -1,27 +1,22 @@
-#pragma once
-#include "../../core/objectlist.hpp"
-#include "../3dSound.hpp"
-#include <string>
+#ifndef TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLIST_HPP
+#define TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLIST_HPP
+
+#include "../../../core/objectlist.hpp"   // Correct path from 'list' folder
+#include "../3dSound.hpp"                  // The 3dSound object itself
+
+template<typename T>
+class ObjectListTableModel;              // Only needed if we ever use table models
 
 class 3dSoundList : public ObjectList<3dSound>
 {
-public:
+  public:
     Method<3dSoundList, std::shared_ptr<3dSound>> create;
 
-    3dSoundList(Object& parent, std::string_view parentPropertyName);
+    explicit 3dSoundList(Object& parent, std::string_view parentPropertyName);
 
-    TableModelPtr getModel() override { return nullptr; } // no table model for now
+    bool isListedProperty(std::string_view name) override;
 
-    void worldEvent(WorldState state, WorldEvent event) override
-    {
-        ObjectList<3dSound>::worldEvent(state, event);
-        const bool editable = contains(state, WorldState::Edit);
-        Attributes::setEnabled(create, editable);
-    }
-
-protected:
-    bool isListedProperty(std::string_view /*name*/) override
-    {
-        return false; // no properties listed for now
-    }
+    void worldEvent(WorldState state, WorldEvent event) override;
 };
+
+#endif

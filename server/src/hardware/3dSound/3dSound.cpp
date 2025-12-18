c46f123431ae95fe66fd0dd6a14f250b8ba19f96
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 21:19:35 +0100

    Update 3dSound.cpp

--- server/src/hardware/3dSound/3dSound.cpp
+++ server/src/hardware/3dSound/3dSound.cpp
@@ -1,84 +1,51 @@
-/**
- * server/src/hardware/3dSound/3dSound.cpp
- *
- * Implements the constructor and property registration for 3D sounds.
- */
-
 #include "3dSound.hpp"
 #include "list/3dSoundList.hpp"
+#include "../../world/world.hpp"
 #include "../../core/attributes.hpp"
 
-Sound3D::Sound3D(World& world, std::string_view _id)
-  : IdObject(world, _id)
-  , name{this, "name", id, PropertyFlags::ReadWrite | PropertyFlags::Store | PropertyFlags::ScriptReadOnly}
-  , controller{this, "controller", nullptr, PropertyFlags::ReadWrite | PropertyFlags::Store,
-      [this](const std::shared_ptr<Sound3DController>& /*newValue*/) { controllerChanged(); }}
-  , x{this, "x", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
-  , y{this, "y", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
-  , z{this, "z", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
-  , volume{this, "volume", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
-  , pitch{this, "pitch", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store}
-  , onEvent{*this, "on_event", EventFlags::Scriptable}
+std::shared_ptr<Sound3D> Sound3D::create(World& world, std::string_view _id)
 {
-  const bool editable = contains(m_world.state.value(), WorldState::Edit);
+    auto sound = std::make_shared<Sound3D>(world, _id);
+    sound->addToWorld();
+    return sound;
+}
 
-  Attributes::addDisplayName(name, "Name");
-  Attributes::addEnabled(name, editable);
-  m_interfaceItems.add(name);
-
-  Attributes::addDisplayName(controller, "Controller");
-  Attributes::addEnabled(controller, editable);
-  Attributes::addObjectList(controller, m_world.sound3DControllers);
-  m_interfaceItems.add(controller);
-
-  Attributes::addEnabled(x, editable);
-  Attributes::addEnabled(y, editable);
-  Attributes::addEnabled(z, editable);
-  m_interfaceItems.add(x);
-  m_interfaceItems.add(y);
-  m_interfaceItems.add(z);
-
-  Attributes::addEnabled(volume, editable);
-  Attributes::addEnabled(pitch, editable);
-  m_interfaceItems.add(volume);
-  m_interfaceItems.add(pitch);
-
-  m_interfaceItems.add(onEvent);
+Sound3D::Sound3D(World& world, std::string_view _id)
+    : Object(world, _id)
+    , name(this, "name", id, PropertyFlags::ReadWrite | PropertyFlags::Store)
+    , x(this, "x", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
+    , y(this, "y", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
+    , z(this, "z", 0.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
+    , volume(this, "volume", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
+    , pitch(this, "pitch", 1.0f, PropertyFlags::ReadWrite | PropertyFlags::Store)
+    , looping(this, "looping", false, PropertyFlags::ReadWrite | PropertyFlags::Store)
+    , play(*this, "play", MethodFlags::ScriptCallable, [this]() { fireEvent("play"); })
+    , stop(*this, "stop", MethodFlags::ScriptCallable, [this]() { fireEvent("stop"); })
+    , pause(*this, "pause", MethodFlags::ScriptCallable, [this]() { fireEvent("pause"); })
+    , onEvent(*this, "on_event", EventFlags::Scriptable)
+{
+    m_interfaceItems.add(name);
+    m_interfaceItems.add(x);
+    m_interfaceItems.add(y);
+    m_interfaceItems.add(z);
+    m_interfaceItems.add(volume);
+    m_interfaceItems.add(pitch);
+    m_interfaceItems.add(looping);
+    m_interfaceItems.add(play);
+    m_interfaceItems.add(stop);
+    m_interfaceItems.add(pause);
+    m_interfaceItems.add(onEvent);
 }
 
 void Sound3D::addToWorld()
 {
-  IdObject::addToWorld();
-  m_world.sound3DList->addObject(shared_ptr<Sound3D>());
+    m_world.get3DSounds()->addObject(shared_from_this());
 }
 
-void Sound3D::loaded()
+void Sound3D::loaded() {}
+void Sound3D::destroying() {}
+
+void Sound3D::fireEvent(const std::string& type)
 {
-  IdObject::loaded();
-  controllerChanged();
+    Object::fireEvent(onEvent, type);
 }
-
-void Sound3D::destroying()
-{
-  m_world.sound3DList->removeObject(shared_ptr<Sound3D>());
-  IdObject::destroying();
-}
-
-void Sound3D::worldEvent(WorldState state, WorldEvent event)
-{
-  IdObject::worldEvent(state, event);
-  const bool editable = contains(state, WorldState::Edit);
-  Attributes::setEnabled(name, editable);
-  Attributes::setEnabled(controller, editable);
-  Attributes::setEnabled(x, editable);
-  Attributes::setEnabled(y, editable);
-  Attributes::setEnabled(z, editable);
-  Attributes::setEnabled(volume, editable);
-  Attributes::setEnabled(pitch, editable);
-}
-
-void Sound3D::controllerChanged()
-{
-  // TODO: implement behavior when controller changes
-}
-

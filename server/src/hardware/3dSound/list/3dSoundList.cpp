5768e7140bd1a1fbc193078181b389f0c319bb1c
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 20:56:21 +0100

    Update 3dSoundList.cpp

--- server/src/hardware/3dSound/list/3dSoundList.cpp
+++ server/src/hardware/3dSound/list/3dSoundList.cpp
@@ -1,1 +1,10 @@
+/**
+ * server/src/hardware/3dSound/list/3dSoundList.cpp
+ *
+ * Implements the list of 3D sounds.
+ */
+
+#include "3dSoundList.hpp"
+
+// Currently no additional implementation required beyond ObjectList
 

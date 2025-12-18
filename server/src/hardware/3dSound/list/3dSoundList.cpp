9e652bdbb0718d17d3395ce52cec71aa94d32b1a
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 21:28:56 +0100

    Update 3dSoundList.cpp

--- server/src/hardware/3dSound/list/3dSoundList.cpp
+++ server/src/hardware/3dSound/list/3dSoundList.cpp
@@ -1,35 +1,39 @@
 #include "3dSoundList.hpp"
-#include "../../world/world.hpp"
+#include "3dSoundListTableModel.hpp"
+#include "../../world/getworld.hpp"
 #include "../../core/attributes.hpp"
 
-Sound3DList::Sound3DList(Object& _parent, std::string_view parentPropertyName)
-    : ObjectList<Sound3D>(_parent, parentPropertyName)
-    , create(*this, "create", [this]()
-    {
-        auto& world = dynamic_cast<World&>(parent());
-        return Sound3D::create(world, world.getUniqueId(Sound3D::defaultId));
-    })
-    , delete_(*this, "delete", [this](const ObjectPtr& obj)
-    {
-        removeObject(obj);
-        return true;
-    })
+3dSoundList::3dSoundList(Object& parent, std::string_view parentPropertyName)
+    : ObjectList<3dSound>(parent, parentPropertyName)
 {
-    m_interfaceItems.add(create);
-    m_interfaceItems.add(delete_);
+    const bool editable = contains(getWorld(parent).state.value(), WorldState::Edit);
+
+    // Example: add "create" method
+    create = Method<3dSoundList, std::shared_ptr<3dSound>>(
+        *this, "create", [this]() {
+            auto& world = getWorld(parent());
+            auto sound = std::make_shared<3dSound>(world, world.getUniqueId("3dSound"));
+            addObject(sound);
+            return sound;
+        });
+
+    Attributes::addDisplayName(create, "Create 3D Sound");
+    Attributes::addEnabled(create, editable);
 }
 
-TableModelPtr Sound3DList::getModel()
+TableModelPtr 3dSoundList::getModel()
 {
-    return {}; // placeholder
+    return std::make_shared<3dSoundListTableModel>(*this);
 }
 
-void Sound3DList::worldEvent(WorldState state, WorldEvent event)
+void 3dSoundList::worldEvent(WorldState state, WorldEvent event)
 {
-    ObjectList<Sound3D>::worldEvent(state, event);
+    ObjectList<3dSound>::worldEvent(state, event);
+    const bool editable = contains(state, WorldState::Edit);
+    Attributes::setEnabled(create, editable);
 }
 
-bool Sound3DList::isListedProperty(std::string_view name)
+bool 3dSoundList::isListedProperty(std::string_view name)
 {
-    return false;
+    return 3dSoundListTableModel::isListedProperty(name);
 }

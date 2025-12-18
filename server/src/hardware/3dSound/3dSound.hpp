c23dfe721f9ae9b9f7194925ddc3a25db0fd3e47
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-19 00:45:20 +0100

    Update 3dSound.hpp

--- server/src/hardware/3dSound/3dSound.hpp
+++ server/src/hardware/3dSound/3dSound.hpp
@@ -1,62 +1,30 @@
-/**
- * server/src/hardware/3dSound/3dSound.hpp
- *
- * This file is part of the traintastic source code.
- *
- * Copyright (C) 2025 Reinder Feenstra
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
-#ifndef TRAINTASTIC_SERVER_HARDWARE_3DSOUND_3DSOUND_HPP
-#define TRAINTASTIC_SERVER_HARDWARE_3DSOUND_3DSOUND_HPP
-
+#pragma once
 #include "../../core/idobject.hpp"
-#include "../../core/property.hpp"
-#include "../../core/method.hpp"
+#include "../../core/objectproperty.tpp"
+#include "../../core/objectvectorproperty.tpp"
+#include "../../world/world.hpp"
+#include <string>
 
-class ThreeDSound : public IdObject
+class 3dSound : public IdObject
 {
-  CLASS_ID("3d_sound")
-  DEFAULT_ID("sound")
-  CREATE(ThreeDSound)
+public:
+    3dSound(World& world, std::string_view _id);
 
-  private:
-    void updateEnabled();
-
-  protected:
-    void addToWorld() override;
-    void loaded() override;
-    void destroying() override;
-    void worldEvent(WorldState state, WorldEvent event) override;
-
-  public:
+    // Properties
     Property<std::string> name;
-    Property<double> positionX;
-    Property<double> positionY;
-    Property<double> positionZ;
-    Property<double> volume;
-    Property<std::string> soundFile;
-    Property<bool> looping;
-    Property<bool> isPlaying;
-    
-    Method<void()> play;
-    Method<void()> stop;
-    Method<void()> test;
+    Property<float> x;
+    Property<float> y;
+    Property<float> z;
+    Property<float> volume;
+    Property<float> pitch;
 
-    ThreeDSound(World& world, std::string_view _id);
+    void addToWorld();
+    void loaded();
+    void destroying();
+
+    // Simple event example
+    Event<> onPlay;
+
+    // Fire event
+    void play();
 };
-
-#endif

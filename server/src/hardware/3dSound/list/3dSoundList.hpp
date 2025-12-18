c384b487fc6b07f0f31358120ade7ac24e415d02
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-18 22:16:09 +0100

    Update 3dSoundList.hpp

--- server/src/hardware/3dSound/list/3dSoundList.hpp
+++ server/src/hardware/3dSound/list/3dSoundList.hpp
@@ -1,34 +1,23 @@
-#include "3dSoundList.hpp"
-#include "../../../world/getworld.hpp"
-#include "../../../core/attributes.hpp"
+#pragma once
 
-3dSoundList::3dSoundList(Object& parent, std::string_view parentPropertyName)
-    : ObjectList<3dSound>(parent, parentPropertyName)
-{
-    const bool editable = contains(getWorld(parent).state.value(), WorldState::Edit);
+// Dependencies
+#include "../../../world/getworld.hpp"       // Adjust path to 'world' folder
+#include "../../../core/objectlist.hpp"       // Core object list
+#include "../../../core/attributes.hpp"       // Attributes, properties, etc.
 
-    // Add "create" method
-    create = Method<3dSoundList, std::shared_ptr<3dSound>>(
-        *this, "create", [this]() {
-            auto& world = getWorld(parent());
-            auto sound = std::make_shared<3dSound>(world, world.getUniqueId("3dSound"));
-            addObject(sound);
-            return sound;
-        });
+// Forward declaration if needed
+class 3dSound;
 
-    Attributes::addDisplayName(create, "Create 3D Sound");
-    Attributes::addEnabled(create, editable);
-}
+// Your class definition
+class 3dSoundList {
+public:
+    // Constructor
+    3dSoundList(World& world);
 
-void 3dSoundList::worldEvent(WorldState state, WorldEvent event)
-{
-    ObjectList<3dSound>::worldEvent(state, event);
-    const bool editable = contains(state, WorldState::Edit);
-    Attributes::setEnabled(create, editable);
-}
+    // Example methods
+    void addSound(3dSound* sound);
+    void removeSound(3dSound* sound);
 
-bool 3dSoundList::isListedProperty(std::string_view name)
-{
-    // For now we just list all properties of 3dSound
-    return name == "name"; // Example property, replace with actual if needed
-}
+private:
+    ObjectList<3dSound> sounds; // or whatever container you use
+};

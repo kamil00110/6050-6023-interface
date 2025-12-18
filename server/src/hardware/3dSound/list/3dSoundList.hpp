4e74f0e5fd7eaa255f8b94143852a6ef83bfa815
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-19 00:12:56 +0100

    Update 3dSoundList.hpp

--- server/src/hardware/3dSound/list/3dSoundList.hpp
+++ server/src/hardware/3dSound/list/3dSoundList.hpp
@@ -1,23 +1,51 @@
-#pragma once
+/**
+ * server/src/hardware/3dSound/list/3dSoundList.hpp
+ *
+ * This file is part of the traintastic source code.
+ *
+ * Copyright (C) 2025 Reinder Feenstra
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
-// Dependencies
-#include "../../../world/getworld.hpp"       // Adjust path to 'world' folder
-#include "../../../core/objectlist.hpp"       // Core object list
-#include "../../../core/attributes.hpp"       // Attributes, properties, etc.
+#ifndef TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLIST_HPP
+#define TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLIST_HPP
 
-// Forward declaration if needed
-class 3dSound;
+#include "../../../core/objectlist.hpp"
+#include "3dSoundListColumn.hpp"
+#include "../../../core/method.hpp"
 
-// Your class definition
-class 3dSoundList {
-public:
-    // Constructor
-    3dSoundList(World& world);
+class ThreeDSound;
 
-    // Example methods
-    void addSound(3dSound* sound);
-    void removeSound(3dSound* sound);
+class ThreeDSoundList : public ObjectList<ThreeDSound>
+{
+  CLASS_ID("list.3d_sound")
 
-private:
-    ObjectList<3dSound> sounds; // or whatever container you use
+  protected:
+    void worldEvent(WorldState state, WorldEvent event) final;
+    bool isListedProperty(std::string_view name) final;
+
+  public:
+    const ThreeDSoundListColumn columns;
+    
+    Method<std::shared_ptr<ThreeDSound>()> create;
+    Method<void(const std::shared_ptr<ThreeDSound>&)> delete_;
+
+    ThreeDSoundList(Object& _parent, std::string_view parentPropertyName, ThreeDSoundListColumn _columns);
+    
+    TableModelPtr getModel() final;
 };
+
+#endif

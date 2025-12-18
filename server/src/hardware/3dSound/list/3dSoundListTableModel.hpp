b452dd2e43259cc2aa073ba4fa0c4aba9aca9da7
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-19 00:14:13 +0100

    Create 3dSoundListTableModel.hpp

---
+++ server/src/hardware/3dSound/list/3dSoundListTableModel.hpp
@@ -0,0 +1,53 @@
+/**
+ * server/src/hardware/3dSound/list/3dSoundListTableModel.hpp
+ *
+ * This file is part of the traintastic source code.
+ *
+ * Copyright (C) 2025 Reinder Feenstra
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLISTTABLEMODEL_HPP
+#define TRAINTASTIC_SERVER_HARDWARE_3DSOUND_LIST_3DSOUNDLISTTABLEMODEL_HPP
+
+#include "../../../core/objectlisttablemodel.hpp"
+#include "3dSoundListColumn.hpp"
+#include "../3dSound.hpp"
+
+class ThreeDSoundList;
+
+class ThreeDSoundListTableModel : public ObjectListTableModel<ThreeDSound>
+{
+  friend class ThreeDSoundList;
+
+  private:
+    std::vector<ThreeDSoundListColumn> m_columns;
+    void changed(uint32_t row, ThreeDSoundListColumn column);
+
+  protected:
+    void propertyChanged(BaseProperty& property, uint32_t row) final;
+
+  public:
+    CLASS_ID("3d_sound_list_table_model")
+    
+    static bool isListedProperty(std::string_view name);
+    
+    ThreeDSoundListTableModel(ThreeDSoundList& list);
+    
+    std::string getText(uint32_t column, uint32_t row) const final;
+};
+
+#endif

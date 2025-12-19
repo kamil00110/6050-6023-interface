6183f0a5da2b200af6597b3322c38152dbaff129
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-19 02:07:18 +0100

    Update world.cpp

--- server/src/world/world.cpp
+++ server/src/world/world.cpp
@@ -350,7 +350,7 @@ World::World(Private /*unused*/) :
   m_interfaceItems.add(scaleRatio);
 
   Attributes::addObjectEditor(threeDSounds, false);
-  m_interfaceItems.add(world.threeDSounds);
+  m_interfaceItems.add(threeDSounds);
 
   m_interfaceItems.add(onlineWhenLoaded);
   m_interfaceItems.add(powerOnWhenLoaded);

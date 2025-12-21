6c7df6d8f7eca3124fe02c654e0afc36279c15ab
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-21 19:34:06 +0100

    Update traintastic.hpp

--- server/src/traintastic/traintastic.hpp
+++ server/src/traintastic/traintastic.hpp
@@ -95,7 +95,7 @@ class Traintastic final : public Object
     std::filesystem::path debugDir() const { return dataDir() / "debug"; }
 
     void importWorld(const std::vector<std::byte>& worldData);
-
+    void logAllAudioDevices();
     RunStatus run(const std::string& worldUUID = {}, bool simulate = false, bool online = false, bool power = false, bool run = false);
     void exit();
 };

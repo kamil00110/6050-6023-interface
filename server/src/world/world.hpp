a6e4c32a27069ffc1636e794ab9de6aebde61bcd
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-22 03:08:08 +0100

    Update world.hpp

--- server/src/world/world.hpp
+++ server/src/world/world.hpp
@@ -36,7 +36,7 @@
 #include "../enum/worldscale.hpp"
 #include "../status/status.hpp"
 #include <traintastic/set/worldstate.hpp>
-#include "../hardware/3dZone/list/3dZoneList.hpp"
+#include "../hardware/3dZone/3dZone.hpp"
 
 class WorldLoader;
 class LNCVProgrammer;

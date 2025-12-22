6132a3636764c42a07e5c45430b67d11c2fb3c94
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-22 07:14:55 +0100

    Update 3dZoneListTableModel.cpp

--- server/src/hardware/3dZone/list/3dZoneListTableModel.cpp
+++ server/src/hardware/3dZone/list/3dZoneListTableModel.cpp
@@ -10,7 +10,8 @@ bool ThreeDZoneListTableModel::isListedProperty(std::string_view name)
     name == "id" ||
     name == "width" ||
     name == "height" ||
-    name == "speaker_setup";
+    name == "speaker_setup" ||
+    name.find("speaker_") == 0;  // Any speaker property
 }
 
 static std::string_view displayName(ThreeDZoneListColumn column)
@@ -20,7 +21,17 @@ static std::string_view displayName(ThreeDZoneListColumn column)
     case ThreeDZoneListColumn::Id: return "Id";
     case ThreeDZoneListColumn::Width: return "Width";
     case ThreeDZoneListColumn::Height: return "Height";
-    case ThreeDZoneListColumn::SpeakerSetup: return "Speaker Setup";
+    case ThreeDZoneListColumn::SpeakerSetup: return "Setup";
+    case ThreeDZoneListColumn::Speaker0: return "Spkr 0";
+    case ThreeDZoneListColumn::Speaker1: return "Spkr 1";
+    case ThreeDZoneListColumn::Speaker2: return "Spkr 2";
+    case ThreeDZoneListColumn::Speaker3: return "Spkr 3";
+    case ThreeDZoneListColumn::Speaker4: return "Spkr 4";
+    case ThreeDZoneListColumn::Speaker5: return "Spkr 5";
+    case ThreeDZoneListColumn::Speaker6: return "Spkr 6";
+    case ThreeDZoneListColumn::Speaker7: return "Spkr 7";
+    case ThreeDZoneListColumn::Speaker8: return "Spkr 8";
+    case ThreeDZoneListColumn::Speaker9: return "Spkr 9";
   }
   assert(false);
   return {};
@@ -36,6 +47,16 @@ static std::string speakerSetupToString(SpeakerSetup setup)
     case SpeakerSetup::Surround9_1: return "9.1";
   }
   return "Unknown";
+}
+
+static std::string formatSpeaker(const SpeakerConfiguration& speaker)
+{
+  std::ostringstream oss;
+  oss << "Vol:" << std::fixed << std::setprecision(1) << (speaker.volumeOverride * 100) << "%";
+  if(!speaker.audioDevice.empty())
+    oss << " Dev:" << speaker.audioDevice;
+  oss << " Ch:" << speaker.audioChannel;
+  return oss.str();
 }
 
 ThreeDZoneListTableModel::ThreeDZoneListTableModel(ThreeDZoneList& list)
@@ -68,21 +89,40 @@ std::string ThreeDZoneListTableModel::getText(uint32_t column, uint32_t row) con
       case ThreeDZoneListColumn::Width:
       {
         std::ostringstream oss;
-        oss << std::fixed << std::setprecision(2);
-        oss << zone.width.value() << " m";
+        oss << std::fixed << std::setprecision(1) << zone.width.value() << "m";
         return oss.str();
       }
       
       case ThreeDZoneListColumn::Height:
       {
         std::ostringstream oss;
-        oss << std::fixed << std::setprecision(2);
-        oss << zone.height.value() << " m";
+        oss << std::fixed << std::setprecision(1) << zone.height.value() << "m";
         return oss.str();
       }
       
       case ThreeDZoneListColumn::SpeakerSetup:
         return speakerSetupToString(zone.speakerSetup.value());
+        
+      case ThreeDZoneListColumn::Speaker0:
+        return formatSpeaker(zone.getSpeaker(0));
+      case ThreeDZoneListColumn::Speaker1:
+        return formatSpeaker(zone.getSpeaker(1));
+      case ThreeDZoneListColumn::Speaker2:
+        return formatSpeaker(zone.getSpeaker(2));
+      case ThreeDZoneListColumn::Speaker3:
+        return formatSpeaker(zone.getSpeaker(3));
+      case ThreeDZoneListColumn::Speaker4:
+        return formatSpeaker(zone.getSpeaker(4));
+      case ThreeDZoneListColumn::Speaker5:
+        return formatSpeaker(zone.getSpeaker(5));
+      case ThreeDZoneListColumn::Speaker6:
+        return formatSpeaker(zone.getSpeaker(6));
+      case ThreeDZoneListColumn::Speaker7:
+        return formatSpeaker(zone.getSpeaker(7));
+      case ThreeDZoneListColumn::Speaker8:
+        return formatSpeaker(zone.getSpeaker(8));
+      case ThreeDZoneListColumn::Speaker9:
+        return formatSpeaker(zone.getSpeaker(9));
     }
     assert(false);
   }
@@ -93,20 +133,30 @@ void ThreeDZoneListTableModel::propertyChanged(BaseProperty& property, uint32_t 
 {
   std::string_view name = property.name();
   
-  if(name == "id") changed(row, ThreeDZoneListColumn::Id);
-  else if(name == "width") changed(row, ThreeDZoneListColumn::Width);
-  else if(name == "height") changed(row, ThreeDZoneListColumn::Height);
-  else if(name == "speaker_setup") changed(row, ThreeDZoneListColumn::SpeakerSetup);
-}
-
-void ThreeDZoneListTableModel::changed(uint32_t row, ThreeDZoneListColumn column)
-{
-  for(size_t i = 0; i < m_columns.size(); i++)
-  {
-    if(m_columns[i] == column)
-    {
-      TableModel::changed(row, static_cast<uint32_t>(i));
-      return;
-    }
-  }
-}
+  if(name == "id") 
+    changed(row, ThreeDZoneListColumn::Id);
+  else if(name == "width") 
+    changed(row, ThreeDZoneListColumn::Width);
+  else if(name == "height") 
+    changed(row, ThreeDZoneListColumn::Height);
+  else if(name == "speaker_setup") 
+    changed(row, ThreeDZoneListColumn::SpeakerSetup);
+  else if(name.find("speaker_0_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker0);
+  else if(name.find("speaker_1_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker1);
+  else if(name.find("speaker_2_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker2);
+  else if(name.find("speaker_3_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker3);
+  else if(name.find("speaker_4_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker4);
+  else if(name.find("speaker_5_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker5);
+  else if(name.find("speaker_6_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker6);
+  else if(name.find("speaker_7_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker7);
+  else if(name.find("speaker_8_") == 0)
+    changed(row, ThreeDZoneListColumn::Speaker8);
+  else if(name.find("speaker_

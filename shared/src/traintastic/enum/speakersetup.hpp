cb0b7b5d04a4c3512dfcc37515d1c98afeaa65cc
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-22 02:04:13 +0100

    Update speakersetup.hpp

--- shared/src/traintastic/enum/speakersetup.hpp
+++ shared/src/traintastic/enum/speakersetup.hpp
@@ -2,6 +2,7 @@
 #define TRAINTASTIC_SHARED_TRAINTASTIC_ENUM_SPEAKERSETUP_HPP
 
 #include <cstdint>
+#include <array>
 
 enum class SpeakerSetup : uint8_t
 {
@@ -18,5 +19,13 @@ TRAINTASTIC_ENUM(SpeakerSetup, "speaker_setup", 4,
   {SpeakerSetup::Surround7_1, "surround_7_1"},
   {SpeakerSetup::Surround9_1, "surround_9_1"},
 });
+
+// ADD THIS: values array for Attributes::addValues()
+constexpr auto speakerSetupValues = std::array<SpeakerSetup, 4>{
+  SpeakerSetup::Quadraphonic,
+  SpeakerSetup::Surround5_1,
+  SpeakerSetup::Surround7_1,
+  SpeakerSetup::Surround9_1,
+};
 
 #endif

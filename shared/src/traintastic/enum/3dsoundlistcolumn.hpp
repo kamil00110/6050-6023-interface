5e27b3055dcc4c08984f3106b8581d7f7cc775b2
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-23 00:24:09 +0100

    Update 3dsoundlistcolumn.hpp

--- shared/src/traintastic/enum/3dsoundlistcolumn.hpp
+++ shared/src/traintastic/enum/3dsoundlistcolumn.hpp
@@ -1,73 +1,54 @@
 /**
- * shared/src/traintastic/enum/3dsoundlistcolumn.hpp
- *
- * This file is part of the traintastic source code.
- *
- * Copyright (C) 2025 Reinder Feenstra
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * shared/src/traintastic/enum/3dzonelistcolumn.hpp
  */
-
-#ifndef TRAINTASTIC_SHARED_TRAINTASTIC_ENUM_3DSOUNDLISTCOLUMN_HPP
-#define TRAINTASTIC_SHARED_TRAINTASTIC_ENUM_3DSOUNDLISTCOLUMN_HPP
+#ifndef TRAINTASTIC_SHARED_TRAINTASTIC_ENUM_3DZONELISTCOLUMN_HPP
+#define TRAINTASTIC_SHARED_TRAINTASTIC_ENUM_3DZONELISTCOLUMN_HPP
 
 #include <cstdint>
 #include <array>
 
-enum class ThreeDSoundListColumn : uint8_t
+enum class ThreeDZoneListColumn : uint8_t
 {
   Id = 1,
-  File = 2,
-  Loop = 4,
-  Volume = 8,
-  Speed = 16,
+  Width = 2,
+  Height = 4,
+  SpeakerSetup = 8,
+  Speakers = 16,
 };
 
-TRAINTASTIC_ENUM(ThreeDSoundListColumn, "3d_sound_list_column", 5,
+TRAINTASTIC_ENUM(ThreeDZoneListColumn, "3d_zone_list_column", 5,
 {
-  {ThreeDSoundListColumn::Id, "id"},
-  {ThreeDSoundListColumn::File, "file"},
-  {ThreeDSoundListColumn::Loop, "loop"},
-  {ThreeDSoundListColumn::Volume, "volume"},
-  {ThreeDSoundListColumn::Speed, "speed"},
+  {ThreeDZoneListColumn::Id, "id"},
+  {ThreeDZoneListColumn::Width, "width"},
+  {ThreeDZoneListColumn::Height, "height"},
+  {ThreeDZoneListColumn::SpeakerSetup, "speaker_setup"},
+  {ThreeDZoneListColumn::Speakers, "speakers"},
 });
 
-constexpr auto threeDSoundListColumnValues = std::array<ThreeDSoundListColumn, 5>{
-  ThreeDSoundListColumn::Id,
-  ThreeDSoundListColumn::File,
-  ThreeDSoundListColumn::Loop,
-  ThreeDSoundListColumn::Volume,
-  ThreeDSoundListColumn::Speed,
+constexpr auto threeDZoneListColumnValues = std::array<ThreeDZoneListColumn, 5>{
+  ThreeDZoneListColumn::Id,
+  ThreeDZoneListColumn::Width,
+  ThreeDZoneListColumn::Height,
+  ThreeDZoneListColumn::SpeakerSetup,
+  ThreeDZoneListColumn::Speakers,
 };
 
-constexpr ThreeDSoundListColumn operator|(ThreeDSoundListColumn lhs, ThreeDSoundListColumn rhs)
+constexpr ThreeDZoneListColumn operator|(ThreeDZoneListColumn lhs, ThreeDZoneListColumn rhs)
 {
-  return static_cast<ThreeDSoundListColumn>(static_cast<uint8_t>(lhs) | static_cast<uint8_t>(rhs));
+  return static_cast<ThreeDZoneListColumn>(static_cast<uint8_t>(lhs) | static_cast<uint8_t>(rhs));
 }
 
-constexpr ThreeDSoundListColumn operator&(ThreeDSoundListColumn lhs, ThreeDSoundListColumn rhs)
+constexpr ThreeDZoneListColumn operator&(ThreeDZoneListColumn lhs, ThreeDZoneListColumn rhs)
 {
-  return static_cast<ThreeDSoundListColumn>(static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs));
+  return static_cast<ThreeDZoneListColumn>(static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs));
 }
 
-constexpr ThreeDSoundListColumn operator~(ThreeDSoundListColumn value)
+constexpr ThreeDZoneListColumn operator~(ThreeDZoneListColumn value)
 {
-  return static_cast<ThreeDSoundListColumn>(~static_cast<uint8_t>(value));
+  return static_cast<ThreeDZoneListColumn>(~static_cast<uint8_t>(value));
 }
 
-constexpr bool contains(ThreeDSoundListColumn value, ThreeDSoundListColumn mask)
+constexpr bool contains(ThreeDZoneListColumn value, ThreeDZoneListColumn mask)
 {
   return (static_cast<uint8_t>(value) & static_cast<uint8_t>(mask)) == static_cast<uint8_t>(mask);
 }

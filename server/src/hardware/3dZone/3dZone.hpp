d848b102bb5cd785e9549cf736fed8afb2078d93
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-23 00:25:08 +0100

    Update 3dZone.hpp

--- server/src/hardware/3dZone/3dZone.hpp
+++ server/src/hardware/3dZone/3dZone.hpp
@@ -1,10 +1,12 @@
+/**
+ * server/src/hardware/3dZone/3dZone.hpp
+ */
 #ifndef TRAINTASTIC_SERVER_HARDWARE_3DZONE_3DZONE_HPP
 #define TRAINTASTIC_SERVER_HARDWARE_3DZONE_3DZONE_HPP
 
 #include "../../core/idobject.hpp"
 #include "../../core/property.hpp"
 #include <traintastic/enum/speakersetup.hpp>
-#include <nlohmann/json.hpp>
 
 class ThreeDZone : public IdObject
 {
@@ -25,13 +27,9 @@ class ThreeDZone : public IdObject
     Property<double> width;
     Property<double> height;
     Property<SpeakerSetup> speakerSetup;
-    Property<std::string> speakersData;  // JSON string containing all speaker configurations
+    Property<std::string> speakersData;
     
     ThreeDZone(World& world, std::string_view _id);
-    
-    // Helper to parse speaker data
-    nlohmann::json getSpeakersJson() const;
-    std::string getSpeakersFormatted() const;
 };
 
 #endif

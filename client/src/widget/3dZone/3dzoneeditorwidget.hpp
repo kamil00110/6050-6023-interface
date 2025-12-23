d9173bf16d7c3da6affde0096fad61431d64c291
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-23 16:57:21 +0100

    Create 3dzoneeditorwidget.hpp

---
+++ client/src/widget/3dZone/3dzoneeditorwidget.hpp
@@ -0,0 +1,44 @@
+#ifndef TRAINTASTIC_CLIENT_WIDGET_3DZONE_3DZONEEDITORWIDGET_HPP
+#define TRAINTASTIC_CLIENT_WIDGET_3DZONE_3DZONEEDITORWIDGET_HPP
+
+#include <QWidget>
+#include "../../network/object.hpp"
+
+class QPaintEvent;
+class QLabel;
+
+class ThreeDZoneEditorWidget : public QWidget
+{
+  Q_OBJECT
+
+public:
+  explicit ThreeDZoneEditorWidget(const ObjectPtr& zone, QWidget* parent = nullptr);
+  
+  QSize sizeHint() const override;
+  QSize minimumSizeHint() const override;
+
+protected:
+  void paintEvent(QPaintEvent* event) override;
+  void resizeEvent(QResizeEvent* event) override;
+
+private slots:
+  void updateFromProperties();
+  void updateSpeakers();
+
+private:
+  ObjectPtr m_zone;
+  double m_width;
+  double m_height;
+  int m_speakerCount;
+  QVector<QPointF> m_speakerPositions;
+  QVector<QString> m_speakerLabels;
+  
+  QRectF m_zoneRect;
+  double m_scale;
+  
+  void calculateLayout();
+  void calculateSpeakerPositions();
+  QPointF worldToScreen(double x, double y) const;
+};
+
+#endif

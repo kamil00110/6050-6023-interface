1ca4b0891285120892c5ac6b34f6edaff4cdfc19
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-23 19:44:12 +0100

    Update 3dzonesubwindow.hpp

--- client/src/subwindow/3dzonesubwindow.hpp
+++ client/src/subwindow/3dzonesubwindow.hpp
@@ -2,28 +2,33 @@
 #define TRAINTASTIC_CLIENT_SUBWINDOW_3DZONE_SUBWINDOW_HPP
 
 #include "subwindow.hpp"
+#include <memory>
 
 class ThreeDZoneEditorWidget;
 class Object;
+
+using ObjectPtr = std::shared_ptr<Object>;
 
 class ThreeDZoneSubWindow final : public SubWindow
 {
     Q_OBJECT
 
 public:
+    // Factory methods
     static ThreeDZoneSubWindow* create(const ObjectPtr& object, QWidget* parent = nullptr);
     static ThreeDZoneSubWindow* create(std::shared_ptr<Connection> connection, const QString& id, QWidget* parent = nullptr);
 
 protected:
     explicit ThreeDZoneSubWindow(std::shared_ptr<Connection> connection, const QString& id, QWidget* parent = nullptr);
 
+    // Implement pure virtual from SubWindow
     QWidget* createWidget(const ObjectPtr& object) override;
 
 private:
-    ObjectPtr m_object;
-    ThreeDZoneEditorWidget* m_editor = nullptr;
+    ObjectPtr m_object;                         // store the object
+    ThreeDZoneEditorWidget* m_editor = nullptr; // pointer to editor widget
 
-    void objectChanged();
+    void objectChanged(); // mark as private, not override
     void buildWidget();
 };
 

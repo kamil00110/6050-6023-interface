847d9e52ac495a531afce0edb253d8d358774274
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-23 19:27:55 +0100

    Update 3dzonesubwindow.hpp

--- client/src/subwindow/3dzonesubwindow.hpp
+++ client/src/subwindow/3dzonesubwindow.hpp
@@ -2,41 +2,23 @@
 #define TRAINTASTIC_CLIENT_SUBWINDOW_3DZONE_SUBWINDOW_HPP
 
 #include "subwindow.hpp"
-#include "../network/object.hpp"
+#include "../network/object.hpp"   // Required for ObjectPtr usage
 
 class ThreeDZoneSubWindow final : public SubWindow
 {
   Q_OBJECT
 
-  public:
-    static ThreeDZoneSubWindow* create(const ObjectPtr& object)
-    {
-      if(!object)
-        return nullptr;
+public:
+    static ThreeDZoneSubWindow* create(const ObjectPtr& object, QWidget* parent = nullptr);
+    static ThreeDZoneSubWindow* create(std::shared_ptr<Connection> connection, const QString& id, QWidget* parent = nullptr);
 
-      return new ThreeDZoneSubWindow(
-        object->connection(),
-        object->getProperty("id")->toString()
-      );
-    }
+protected:
+    explicit ThreeDZoneSubWindow(const ObjectPtr& object, QWidget* parent = nullptr);
+    explicit ThreeDZoneSubWindow(std::shared_ptr<Connection> connection, const QString& id, QWidget* parent = nullptr);
 
-    static ThreeDZoneSubWindow* create(
-      std::shared_ptr<Connection> connection,
-      const QString& id)
-    {
-      return new ThreeDZoneSubWindow(std::move(connection), id);
-    }
-
-  protected:
-    explicit ThreeDZoneSubWindow(
-      std::shared_ptr<Connection> connection,
-      const QString& id,
-      QWidget* parent = nullptr)
-      : SubWindow(SubWindowType::ThreeDZone, std::move(connection), id, parent)
-    {
-    }
-
-    QWidget* createWidget(const ObjectPtr& object) override;
+    void objectChanged() override;
+    void buildWidget();
 };
 
 #endif
+

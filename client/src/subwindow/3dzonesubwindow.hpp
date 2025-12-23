96c3de992577e152703f1b256e175e5303dc7078
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-23 19:48:02 +0100

    Update 3dzonesubwindow.hpp

--- client/src/subwindow/3dzonesubwindow.hpp
+++ client/src/subwindow/3dzonesubwindow.hpp
@@ -1,35 +1,30 @@
-#ifndef TRAINTASTIC_CLIENT_SUBWINDOW_3DZONE_SUBWINDOW_HPP
-#define TRAINTASTIC_CLIENT_SUBWINDOW_3DZONE_SUBWINDOW_HPP
+#ifndef TRAINTASTIC_CLIENT_SUBWINDOW_3DZONESUBWINDOW_HPP
+#define TRAINTASTIC_CLIENT_SUBWINDOW_3DZONESUBWINDOW_HPP
 
 #include "subwindow.hpp"
-#include <memory>
 
 class ThreeDZoneEditorWidget;
-class Object;
 
-using ObjectPtr = std::shared_ptr<Object>;
-
-class ThreeDZoneSubWindow final : public SubWindow
+class ThreeDZoneSubWindow : public SubWindow
 {
-    Q_OBJECT
-
-public:
-    // Factory methods
-    static ThreeDZoneSubWindow* create(const ObjectPtr& object, QWidget* parent = nullptr);
-    static ThreeDZoneSubWindow* create(std::shared_ptr<Connection> connection, const QString& id, QWidget* parent = nullptr);
+  Q_OBJECT
 
 protected:
-    explicit ThreeDZoneSubWindow(std::shared_ptr<Connection> connection, const QString& id, QWidget* parent = nullptr);
+  ThreeDZoneSubWindow(const ObjectPtr& object, QWidget* parent = nullptr);
+  ThreeDZoneSubWindow(const std::shared_ptr<Connection>& connection, const QString& id, QWidget* parent = nullptr);
 
-    // Implement pure virtual from SubWindow
-    QWidget* createWidget(const ObjectPtr& object) override;
+public:
+  static ThreeDZoneSubWindow* create(const ObjectPtr& object, QWidget* parent = nullptr);
+  static ThreeDZoneSubWindow* create(const std::shared_ptr<Connection>& connection, const QString& id, QWidget* parent = nullptr);
+
+  SubWindowType type() const final { return SubWindowType::ThreeDZone; }
+
+protected:
+  void objectChanged() final;
+  void buildWidget() final;
 
 private:
-    ObjectPtr m_object;                         // store the object
-    ThreeDZoneEditorWidget* m_editor = nullptr; // pointer to editor widget
-
-    void objectChanged(); // mark as private, not override
-    void buildWidget();
+  ThreeDZoneEditorWidget* m_editor;
 };
 
 #endif

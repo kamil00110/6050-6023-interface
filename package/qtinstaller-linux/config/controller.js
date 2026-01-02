270072cc9651feb9a61114bd755809167928904b
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-01-03 00:12:28 +0100

    Add files via upload

--- package/qtinstaller-linux/config/controller.js
+++ package/qtinstaller-linux/config/controller.js
@@ -2,6 +2,50 @@ function Controller() {
     // Enable verbose installer output for debugging
     installer.setValue("verbose", "true");
     
+    // Check if Traintastic is already installed
+    var existingInstall = installer.value("HKEY_LOCAL_MACHINE\\SOFTWARE\\traintastic.org\\Traintastic\\InstallLocation");
+    
+    if (installer.isInstaller() && existingInstall && existingInstall !== "") {
+        console.log("Existing installation detected at: " + existingInstall);
+        
+        // Check if maintenance tool exists
+        var maintenanceTool = existingInstall + "/TraintasticMaintenanceTool.exe";
+        
+        if (installer.fileExists(maintenanceTool)) {
+            // Launch maintenance tool and exit installer
+            console.log("Launching maintenance tool: " + maintenanceTool);
+            
+            // Show message to user
+            var result = QMessageBox.information(
+                "existingInstallation",
+                "Traintastic Already Installed",
+                "Traintastic is already installed on this computer.\n\n" +
+                "Click OK to open the Maintenance Tool where you can:\n" +
+                "• Update to a newer version\n" +
+                "• Modify installed components\n" +
+                "• Repair the installation\n" +
+                "• Uninstall Traintastic\n\n" +
+                "Click Cancel to continue with a new installation (not recommended).",
+                QMessageBox.Ok | QMessageBox.Cancel
+            );
+            
+            if (result === QMessageBox.Ok) {
+                installer.execute(maintenanceTool, []);
+                installer.setDefaultPageVisible(QInstaller.Introduction, false);
+                installer.setDefaultPageVisible(QInstaller.TargetDirectory, false);
+                installer.setDefaultPageVisible(QInstaller.ComponentSelection, false);
+                installer.setDefaultPageVisible(QInstaller.LicenseCheck, false);
+                installer.setDefaultPageVisible(QInstaller.ReadyForInstallation, false);
+                installer.setDefaultPageVisible(QInstaller.PerformInstallation, false);
+                installer.setDefaultPageVisible(QInstaller.InstallationFinished, false);
+                
+                // Exit gracefully
+                gui.clickButton(buttons.CancelButton);
+                return;
+            }
+        }
+    }
+    
     // Check if this is an update/maintenance run
     if (installer.isInstaller()) {
         installer.autoRejectMessageBoxes();
@@ -15,13 +59,19 @@ Controller.prototype.IntroductionPageCallback = function() {
     if (widget != null) {
         if (installer.isInstaller()) {
             widget.title = "Welcome to Traintastic Installer";
-            widget.MessageLabel.setText("This will install Traintastic on your computer.");
+            if (widget.MessageLabel) {
+                widget.MessageLabel.setText("This will install Traintastic on your computer.");
+            }
         } else if (installer.isUninstaller()) {
             widget.title = "Uninstall Traintastic";
-            widget.MessageLabel.setText("This will remove Traintastic from your computer.");
+            if (widget.MessageLabel) {
+                widget.MessageLabel.setText("This will remove Traintastic from your computer.");
+            }
         } else if (installer.isUpdater()) {
             widget.title = "Update Traintastic";
-            widget.MessageLabel.setText("This will update Traintastic to the latest version.");
+            if (widget.MessageLabel) {
+                widget.MessageLabel.setText("This will update Traintastic to the latest version.");
+            }
         }
     }
 }
@@ -44,10 +94,14 @@ Controller.prototype.FinishedPageCallback = function() {
     var widget = gui.currentPageWidget();
     if (widget != null) {
         if (installer.isInstaller() || installer.isUpdater()) {
-            widget.RunItCheckBox.visible = true;
-            widget.RunItCheckBox.checked = false;
+            if (widget.RunItCheckBox) {
+                widget.RunItCheckBox.visible = true;
+                widget.RunItCheckBox.checked = false;
+            }
         } else if (installer.isUninstaller()) {
-            widget.RunItCheckBox.visible = false;
+            if (widget.RunItCheckBox) {
+                widget.RunItCheckBox.visible = false;
+            }
         }
     }
 }

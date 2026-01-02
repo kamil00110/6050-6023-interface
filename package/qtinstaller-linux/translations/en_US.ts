0d10e35a616789d314498b2a97deaf4f1e8ae719
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-01-02 15:38:04 +0100

    Add files via upload

---
+++ package/qtinstaller-linux/translations/en_US.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>FirewallPage</name>
+    <message>
+        <source>Additional Options</source>
+        <translation>Additional Options</translation>
+    </message>
+    <message>
+        <source>Windows Firewall:</source>
+        <translation>Windows Firewall:</translation>
+    </message>
+    <message>
+        <source>Allow Traintastic client</source>
+        <translation>Allow Traintastic client</translation>
+    </message>
+    <message>
+        <source>Allow WLANmaus/Z21</source>
+        <translation>Allow WLANmaus/Z21</translation>
+    </message>
+    <message>
+        <source>Shortcuts:</source>
+        <translation>Shortcuts:</translation>
+    </message>
+    <message>
+        <source>Create desktop shortcuts</source>
+        <translation>Create desktop shortcuts</translation>
+    </message>
+</context>
+<context>
+    <name>org.traintastic.server</name>
+    <message>
+        <source>Traintastic Server</source>
+        <translation>Traintastic Server</translation>
+    </message>
+    <message>
+        <source>Server component for the computer connected to the command station.</source>
+        <translation>Server component for the computer connected to the command station.</translation>
+    </message>
+</context>
+<context>
+    <name>org.traintastic.client</name>
+    <message>
+        <source>Traintastic Client</source>
+        <translation>Traintastic Client</translation>
+    </message>
+    <message>
+        <source>Client component for operating the model railway layout.</source>
+        <translation>Client component for operating the model railway layout.</translation>
+    </message>
+</context>
+<context>
+    <name>org.traintastic.shared</name>
+    <message>
+        <source>Traintastic Shared Data</source>
+        <translation>Traintastic Shared Data</translation>
+    </message>
+    <message>
+        <source>Shared data files (translations, manual, LNCV definitions)</source>
+        <translation>Shared data files (translations, manual, LNCV definitions)</translation>
+    </message>
+</context>
+<context>
+    <name>Controller</name>
+    <message>
+        <source>Welcome to Traintastic Installer</source>
+        <translation>Welcome to Traintastic Installer</translation>
+    </message>
+    <message>
+        <source>This will install Traintastic on your computer.</source>
+        <translation>This will install Traintastic on your computer.</translation>
+    </message>
+    <message>
+        <source>Select Components</source>
+        <translation>Select Components</translation>
+    </message>
+    <message>
+        <source>Select which components to install</source>
+        <translation>Select which components to install</translation>
+    </message>
+    <message>
+        <source>Launch Traintastic Client</source>
+        <translation>Launch Traintastic Client</translation>
+    </message>
+</context>
+</TS>

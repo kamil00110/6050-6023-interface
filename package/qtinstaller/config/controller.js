027ce9af055be802c84297c921af5abd0c730f40
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-01-02 02:26:19 +0100

    Update controller.js

--- package/qtinstaller/config/controller.js
+++ package/qtinstaller/config/controller.js
@@ -2,49 +2,33 @@ function Controller() {
     installer.autoRejectMessageBoxes();
     installer.setMessageBoxAutomaticAnswer("OverwriteTargetDirectory", QMessageBox.Yes);
     installer.setMessageBoxAutomaticAnswer("stopProcessesForUpdates", QMessageBox.Ignore);
-    
-    // Add custom component selection page
-    installer.addWizardPage(component, "ComponentSelectionPage", QInstaller.TargetDirectory);
-}
-
-Controller.prototype.ComponentSelectionPageCallback = function() {
-    var page = gui.pageWidgetByObjectName("DynamicComponentSelectionPage");
-    page.entered.connect(this, Controller.prototype.ComponentSelectionPageEntered);
-}
-
-Controller.prototype.ComponentSelectionPageEntered = function() {
-    var page = gui.currentPageWidget();
-    if (page != null) {
-        page.title = qsTr("Select Components");
-        page.subTitle = qsTr("Select which components to install");
-    }
 }
 
 Controller.prototype.IntroductionPageCallback = function() {
     var widget = gui.currentPageWidget();
     if (widget != null) {
-        widget.title = qsTr("Welcome to Traintastic Installer");
-        widget.MessageLabel.setText(qsTr("This will install Traintastic on your computer."));
+        widget.title = "Welcome to Traintastic Installer";
     }
 }
 
 Controller.prototype.TargetDirectoryPageCallback = function() {
-    gui.currentPageWidget().TargetDirectoryLineEdit.setText(installer.value("TargetDir"));
+    var widget = gui.currentPageWidget();
+    if (widget != null) {
+        widget.TargetDirectoryLineEdit.setText(installer.value("TargetDir"));
+    }
 }
 
 Controller.prototype.ComponentSelectionPageCallback = function() {
     var widget = gui.currentPageWidget();
-    widget.selectAll();
-}
-
-Controller.prototype.ReadyForInstallationPageCallback = function() {
-    gui.currentPageWidget().showAll();
+    if (widget != null) {
+        widget.selectAll();
+    }
 }
 
 Controller.prototype.FinishedPageCallback = function() {
-    var checkBoxForm = gui.currentPageWidget().LaunchQtCreatorCheckBoxForm;
-    if (checkBoxForm && installer.isInstaller()) {
-        checkBoxForm.launchQtCreatorCheckBox.text = qsTr("Launch Traintastic Client");
-        checkBoxForm.launchQtCreatorCheckBox.checked = false;
+    var widget = gui.currentPageWidget();
+    if (widget != null && installer.isInstaller()) {
+        widget.RunItCheckBox.visible = true;
+        widget.RunItCheckBox.checked = false;
     }
 }

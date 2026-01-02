fb8826a20e8f129bfecfdebe13b2062b44ffaa9f
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-01-02 15:27:50 +0100

    Create installscript.js

---
+++ package/qtinstaller-linux/packages/org.traintastic.server/meta/installscript.js
@@ -0,0 +1,111 @@
+function Component() {
+    // Constructor
+}
+
+Component.prototype.createOperations = function() {
+    try {
+        // Call default implementation
+        component.createOperations();
+        
+        var targetDir = installer.value("TargetDir");
+        var homeDir = installer.value("HomeDir");
+        var serverBin = targetDir + "/bin/traintastic-server";
+        var maintenanceTool = targetDir + "/TraintasticMaintenanceTool";
+        
+        // Make server executable
+        component.addOperation("Execute", "chmod", "+x", serverBin);
+        
+        // Create desktop entry for server
+        var desktopEntry = "[Desktop Entry]\n" +
+            "Type=Application\n" +
+            "n=Traintastic Server\n" +
+            "Comment=Model railway control system server\n" +
+            "Exec=" + serverBin + " --tray\n" +
+            "Icon=" + targetDir + "/share/pixmaps/traintastic-server.png\n" +
+            "Categories=Utility;Application;\n" +
+            "Terminal=false\n";
+        
+        var desktopFile = homeDir + "/.local/share/applications/traintastic-server.desktop";
+        
+        component.addOperation("Mkdir", homeDir + "/.local/share/applications");
+        component.addOperation("AppendFile", desktopFile, desktopEntry);
+        component.addOperation("Execute", "chmod", "+x", desktopFile);
+        
+        // Create desktop entry for maintenance tool
+        var maintenanceEntry = "[Desktop Entry]\n" +
+            "Type=Application\n" +
+            "n=Modify, Repair or Uninstall Traintastic\n" +
+            "Comment=Update, modify, repair or uninstall Traintastic\n" +
+            "Exec=" + maintenanceTool + "\n" +
+            "Icon=" + targetDir + "/share/pixmaps/traintastic.png\n" +
+            "Categories=System;Settings;\n" +
+            "Terminal=false\n";
+        
+        var maintenanceDesktopFile = homeDir + "/.local/share/applications/traintastic-maintenance.desktop";
+        component.addOperation("AppendFile", maintenanceDesktopFile, maintenanceEntry);
+        component.addOperation("Execute", "chmod", "+x", maintenanceDesktopFile);
+        
+        // Create server settings file if it doesn't exist (only on initial install)
+        if (installer.isInstaller()) {
+            var configDir = homeDir + "/.config/traintastic/server";
+            var settingsPath = configDir + "/settings.json";
+            
+            if (!installer.fileExists(settingsPath)) {
+                var language = getTraintasticLanguage();
+                var settingsContent = '{"language":"' + language + '"}';
+                
+                component.addOperation("Mkdir", configDir);
+                component.addOperation("AppendFile", settingsPath, settingsContent);
+            }
+        }
+        
+        // Optional: Create systemd service file
+        var page = component.userInterface("LinuxOptionsPage");
+        if (page && page.createSystemdService && page.createSystemdService.checked) {
+            var serviceFile = homeDir + "/.config/systemd/user/traintastic-server.service";
+            var serviceContent = "[Unit]\n" +
+                "Description=Traintastic Server\n" +
+                "After=network.target\n" +
+                "\n" +
+                "[Service]\n" +
+                "Type=simple\n" +
+                "ExecStart=" + serverBin + "\n" +
+                "Restart=on-failure\n" +
+                "RestartSec=5\n" +
+                "\n" +
+                "[Install]\n" +
+                "WantedBy=default.target\n";
+            
+            component.addOperation("Mkdir", homeDir + "/.config/systemd/user");
+            component.addOperation("AppendFile", serviceFile, serviceContent);
+            
+            if (page.enableSystemdService && page.enableSystemdService.checked) {
+                component.addOperation("Execute", "systemctl", "--user", "daemon-reload");
+                component.addOperation("Execute", "systemctl", "--user", "enable", "traintastic-server.service");
+                component.addOperation("Execute", "systemctl", "--user", "start", "traintastic-server.service");
+            }
+        }
+        
+    } catch (e) {
+        console.log("Error in server createOperations: " + e);
+    }
+}
+
+function getTraintasticLanguage() {
+    var locale = installer.value("Locale");
+    
+    if (locale.indexOf("nl") === 0)
+        return "nl-nl";
+    else if (locale.indexOf("de") === 0)
+        return "de-de";
+    else if (locale.indexOf("it") === 0)
+        return "it-it";
+    else if (locale.indexOf("sv") === 0)
+        return "sv-se";
+    else if (locale.indexOf("fr") === 0)
+        return "fr-fr";
+    else if (locale.indexOf("pl") === 0)
+        return "pl-pl";
+    else
+        return "en-us";
+}

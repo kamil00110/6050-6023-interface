c64ab0368cb70d0277e47fba73e32bd665d88297
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-01-05 22:15:30 +0100

    Create installer.js

---
+++ package/qtinstaller/packages/org.traintastic.server.firewall/meta/installer.js
@@ -0,0 +1,40 @@
+function Component() {}
+
+Component.prototype.createOperations = function() {
+    component.createOperations();
+    
+    if (systemInfo.kernelType !== "winnt") return;
+    
+    var targetDir = installer.value("TargetDir");
+    var serverExe = targetDir + "/server/traintastic-server.exe";
+    
+    console.log("Adding firewall rules for Traintastic client connections...");
+    
+    // TCP rule
+    component.addElevatedOperation("Execute",
+        "{0,1}",
+        "netsh", "advfirewall", "firewall", "add", "rule",
+        "name=Traintastic server (TCP)",
+        "dir=in",
+        "program=" + serverExe,
+        "protocol=TCP",
+        "localport=5740",
+        "action=allow",
+        "UNDOEXECUTE",
+        "netsh", "advfirewall", "firewall", "delete", "rule",
+        "name=Traintastic server (TCP)");
+    
+    // UDP rule
+    component.addElevatedOperation("Execute",
+        "{0,1}",
+        "netsh", "advfirewall", "firewall", "add", "rule",
+        "name=Traintastic server (UDP)",
+        "dir=in",
+        "program=" + serverExe,
+        "protocol=UDP",
+        "localport=5740",
+        "action=allow",
+        "UNDOEXECUTE",
+        "netsh", "advfirewall", "firewall", "delete", "rule",
+        "name=Traintastic server (UDP)");
+}

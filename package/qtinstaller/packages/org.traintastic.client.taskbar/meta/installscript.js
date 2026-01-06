cd92ea8ed701d3b8f334efe822673ec04b61f6cc
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-01-06 18:21:22 +0100

    Update installscript.js

--- package/qtinstaller/packages/org.traintastic.client.taskbar/meta/installscript.js
+++ package/qtinstaller/packages/org.traintastic.client.taskbar/meta/installscript.js
@@ -10,16 +10,16 @@ Component.prototype.createOperations = function() {
     
     console.log("Pinning client to taskbar...");
     
-    var psCommand = '$shell = New-Object -ComObject Shell.Application; ' +
-                   '$folder = $shell.Namespace(\\"' + targetDir.replace(/\//g, "\\\\") + '\\\\client\\"); ' +
-                   '$item = $folder.ParseName(\\"traintastic-client.exe\\"); ' +
-                   '$verb = $item.Verbs() | Where-Object {$_.Name -match \\"Pin to taskbar\\" -or $_.Name -match \\"An Taskleiste\\"}; ' +
-                   'if($verb) { $verb.DoIt() }';
+    // Windows 10/11: Copy shortcut to taskbar folder
+    var appData = installer.value("UserProfile") + "/AppData/Roaming";
+    var taskbarDir = appData + "/Microsoft/Internet Explorer/Quick Launch/User Pinned/TaskBar";
     
-    component.addOperation("Execute",
-        "{0,1}",
-        "powershell",
-        "-NoProfile",
-        "-ExecutionPolicy", "Bypass",
-        "-Command", psCommand);
+    // Create the shortcut in taskbar folder
+    component.addOperation("CreateShortcut",
+        clientExe,
+        taskbarDir + "/Traintastic Client.lnk",
+        "workingDirectory=" + targetDir + "/client",
+        "iconPath=" + clientExe,
+        "iconId=0",
+        "description=Traintastic Client");
 }

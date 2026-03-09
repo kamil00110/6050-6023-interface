64e6dad8449fed2ce9ab1683a6e838f72c22ccf8
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-09 17:39:24 +0100

    Refactor Kernel class and update method signatures

--- server/src/hardware/protocol/Marklin6050Interface/kernel.hpp
+++ server/src/hardware/protocol/Marklin6050Interface/kernel.hpp
@@ -1,51 +1,78 @@
+/**
+ * server/src/hardware/protocol/Marklin6050Interface/kernel.hpp
+ *
+ * Copyright (C) 2025
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 #pragma once
+
 #include <string>
 #include <cstdint>
 #include <thread>
 #include <functional>
 #include <atomic>
 
-#include "../../output/outputvalue.hpp"   
+#include "protocol.hpp"
+#include "../../output/outputvalue.hpp"
+
 namespace Marklin6050 {
 
-class Kernel {
+class Kernel
+{
 public:
     Kernel(const std::string& port, unsigned int baudrate = 2400);
     ~Kernel();
 
-    int readByte();               
+    // --- Serial I/O ---
     bool sendByte(unsigned char byte);
-    bool setAccessory(uint32_t address, OutputValue value, unsigned int timeMs);
-    void setBaudRate(unsigned int baud) { m_baudrate = baud; }
+    int readByte();
+
+    // --- Lifecycle ---
     bool start();
     void stop();
     bool isRunning() const { return m_running.load(); }
-    void startInputThread(unsigned int moduleCount, unsigned int intervalMs);
-    void stopInputThread();
-    std::function<void(uint32_t, bool)> s88Callback;
+
+    // --- Configuration ---
+    void setRedundancy(unsigned int count);
+
+    // --- Loco commands ---
     void setLocoSpeed(uint8_t address, uint8_t speed, bool f0);
     void setLocoDirection(uint8_t address, bool f0);
-    void setLocoFunction(uint8_t address, uint8_t currentSpeed, bool f0);
-    void setRedundancy(unsigned int count);
-    void setLocoFunctions1to4(uint8_t address, bool f1, bool f2, bool f3, bool f4);
     void setLocoEmergencyStop(uint8_t address, bool f0);
+    void setLocoFunction(uint8_t address, uint8_t currentSpeed, bool f0);
+    void setLocoFunctions1to4(uint8_t address, bool f1, bool f2, bool f3, bool f4);
+
+    // --- Accessory commands ---
+    bool setAccessory(uint32_t address, OutputValue value, unsigned int timeMs);
+
+    // --- S88 input polling ---
+    void startInputThread(unsigned int moduleCount, unsigned int intervalMs);
+    void stopInputThread();
+
+    // --- Callbacks ---
+    std::function<void(uint32_t, bool)> s88Callback;
 
 private:
     std::string m_port;
     unsigned int m_baudrate;
-    std::thread m_inputThread;          
-    std::atomic<bool> m_running{false}; 
-    void inputLoop(unsigned int modules);
     unsigned int m_redundancy{0};
-    void sendCommand(uint8_t byte1, uint8_t byte2);
+    std::thread m_inputThread;
+    std::atomic<bool> m_running{false};
+    bool m_isOpen{false};
 
+    void sendCommand(uint8_t byte1, uint8_t byte2);
+    void inputLoop(unsigned int modules);
 
 #if defined(_WIN32)
     void* m_handle;
 #else
     int m_fd;
 #endif
-    bool m_isOpen;
 };
 
-} 
+} // namespace Marklin6050

5f8a44efee08efa129f44040e6623a56ed74a7a0
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-09 15:44:14 +0100

    Add locomotive control functions and redundancy setting

--- server/src/hardware/protocol/Marklin6050Interface/kernel.hpp
+++ server/src/hardware/protocol/Marklin6050Interface/kernel.hpp
@@ -23,6 +23,11 @@ public:
     void startInputThread(unsigned int moduleCount, unsigned int intervalMs);
     void stopInputThread();
     std::function<void(uint32_t, bool)> s88Callback;
+    void setLocoSpeed(uint8_t address, uint8_t speed, bool f0);
+    void setLocoDirection(uint8_t address, bool f0);
+    void setLocoFunction(uint8_t address, uint8_t currentSpeed, bool f0);
+    void setRedundancy(unsigned int count);
+    void setLocoFunctions1to4(uint8_t address, bool f1, bool f2, bool f3, bool f4);
 
 private:
     std::string m_port;
@@ -30,7 +35,8 @@ private:
     std::thread m_inputThread;          
     std::atomic<bool> m_running{false}; 
     void inputLoop(unsigned int modules);
-    
+    unsigned int m_redundancy{0};
+    void sendLocoCommand(uint8_t address, uint8_t speedByte, bool f0);    
 
 
 

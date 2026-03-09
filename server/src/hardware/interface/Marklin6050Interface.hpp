a950b652e23d21ce699ce1da87ff76ad3d41c442
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-09 17:19:49 +0100

    Update Marklin6050Interface.hpp

--- server/src/hardware/interface/Marklin6050Interface.hpp
+++ server/src/hardware/interface/Marklin6050Interface.hpp
@@ -1,75 +1,70 @@
+/**
+ * server/src/hardware/interface/Marklin6050Interface.hpp
+ *
+ * Copyright (C) 2025
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 #ifndef TRAINTASTIC_SERVER_HARDWARE_INTERFACE_MARKLIN6050INTERFACE_HPP
 #define TRAINTASTIC_SERVER_HARDWARE_INTERFACE_MARKLIN6050INTERFACE_HPP
-#include <thread>
-#include <atomic>
-#include <chrono>
 
 #include "interface.hpp"
 #include "../../core/objectproperty.hpp"
 #include "../output/outputcontroller.hpp"
 #include "../input/inputcontroller.hpp"
 #include "../decoder/decodercontroller.hpp"
-#include "../../hardware/protocol/Marklin6050Interface/serial.hpp"
 #include "../../core/serialdeviceproperty.hpp"
+#include "../../hardware/protocol/Marklin6050Interface/serial.hpp"
 #include "../../hardware/protocol/Marklin6050Interface/kernel.hpp"
+#include "../../hardware/protocol/Marklin6050Interface/settings.hpp"
 
-class Marklin6050Interface 
-: public Interface
-, public OutputController
-, public InputController
-, public DecoderController
+class Marklin6050Interface
+    : public Interface
+    , public OutputController
+    , public InputController
+    , public DecoderController
 {
-  CLASS_ID("interface.marklin6050")
-  DEFAULT_ID("marklin6050")
-  CREATE_DEF(Marklin6050Interface)
+    CLASS_ID("interface.marklin6050")
+    DEFAULT_ID("marklin6050")
+    CREATE_DEF(Marklin6050Interface)
 
 private:
-  SerialDeviceProperty serialPort;
-  Property<uint32_t> baudrate;
-  Property<unsigned int> centralUnitVersion;
-  Property<bool> analog;
-  Property<unsigned int> s88amount;
-  Property<unsigned int> s88interval;
-  Property<unsigned int> turnouttime;
-  Property<unsigned int> redundancy;
-  Property<bool> extensions;
-  Property<unsigned int> oldAddress;
-  Property<unsigned int> newAddress;
-  Property<bool> programmer;
-  std::unique_ptr<Marklin6050::Kernel> m_kernel;
-  void updateEnabled();
-  void serialPortChanged(const std::string& newPort);
-  std::vector<bool> m_lastS88State;
-  std::thread m_s88Thread;
-  std::atomic<bool> m_runS88{false};
+    std::unique_ptr<Marklin6050::Kernel> m_kernel;
+
+    void updateEnabled();
     void onS88Input(uint32_t address, bool state);
 
-
 protected:
-  void addToWorld() final;
-  void loaded() final;
-  void destroying() final;
-  void worldEvent(WorldState state, WorldEvent event) final;
-  void onlineChanged(bool value); 
-  void onCentralUnitVersionChanged();
-  bool setOnline(bool& value, bool simulation) final;
+    void addToWorld() final;
+    void loaded() final;
+    void destroying() final;
+    void worldEvent(WorldState state, WorldEvent event) final;
+    void onlineChanged(bool value);
+    bool setOnline(bool& value, bool simulation) final;
 
 public:
-  Marklin6050Interface(World& world, std::string_view id);
-  
- 
-   // DecoderController:
+    SerialDeviceProperty serialPort;
+    Property<uint32_t> baudrate;
+    ObjectProperty<Marklin6050::Settings> settings;
+
+    Marklin6050Interface(World& world, std::string_view id);
+
+    // DecoderController:
     std::span<const DecoderProtocol> decoderProtocols() const final;
     std::pair<uint16_t, uint16_t> decoderAddressMinMax(DecoderProtocol protocol) const final;
     std::span<const uint8_t> decoderSpeedSteps(DecoderProtocol protocol) const final;
     void decoderChanged(const Decoder& decoder, DecoderChangeFlags changes, uint32_t functionNumber) final;
-    void checkDecoder(const Decoder& decoder);
-  // InputController:
+
+    // InputController:
     std::span<const InputChannel> inputChannels() const final;
     std::pair<uint32_t, uint32_t> inputAddressMinMax(InputChannel channel) const final;
     void inputSimulateChange(InputChannel channel, uint32_t address, SimulateInputAction action) final;
 
-  // OutputController:
+    // OutputController:
     std::span<const OutputChannel> outputChannels() const final;
     std::pair<uint32_t, uint32_t> outputAddressMinMax(OutputChannel channel) const final;
     [[nodiscard]] bool setOutputValue(OutputChannel channel, uint32_t address, OutputValue value) final;

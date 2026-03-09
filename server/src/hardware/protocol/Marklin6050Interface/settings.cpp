ed4f4e9870b9d70192bc8fc382a643a37c70cca0
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-09 18:09:23 +0100

    Refactor Marklin 6050 settings for improved clarity

--- server/src/hardware/protocol/Marklin6050Interface/settings.cpp
+++ server/src/hardware/protocol/Marklin6050Interface/settings.cpp
@@ -1,3 +1,14 @@
+/**
+ * server/src/hardware/protocol/Marklin6050Interface/settings.cpp
+ *
+ * Copyright (C) 2025
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 #include "settings.hpp"
 #include "../../../core/attributes.hpp"
 #include "../../../utils/displayname.hpp"
@@ -26,9 +37,9 @@ Settings::Settings(Object& _parent, std::string_view parentPropertyName)
     static const std::vector<std::string_view> cuLabels = {
         "6020", "6021", "6022", "6023", "6223", "6027", "6029", "6030", "6032"
     };
-    Attributes::addCategory(centralUnitVersion, "Märklin 6050");
-    Attributes::addDisplayName(centralUnitVersion, "Central Unit Version");
-    Attributes::addHelp(centralUnitVersion, "CUversion");
+    Attributes::addCategory(centralUnitVersion, "category:marklin_6050");
+    Attributes::addDisplayName(centralUnitVersion, DisplayName::Marklin6050::centralUnitVersion);
+    Attributes::addHelp(centralUnitVersion, "marklin6050_settings:central_unit_version.help");
     Attributes::addEnabled(centralUnitVersion, true);
     Attributes::addVisible(centralUnitVersion, true);
     Attributes::addValues(centralUnitVersion, cuOptions);
@@ -36,15 +47,17 @@ Settings::Settings(Object& _parent, std::string_view parentPropertyName)
     m_interfaceItems.add(centralUnitVersion);
 
     // --- Analog mode ---
-    Attributes::addCategory(analog, "Märklin 6050");
-    Attributes::addDisplayName(analog, "Analog mode");
+    Attributes::addCategory(analog, "category:marklin_6050");
+    Attributes::addDisplayName(analog, DisplayName::Marklin6050::analog);
+    Attributes::addHelp(analog, "marklin6050_settings:analog.help");
     Attributes::addEnabled(analog, false);
     Attributes::addVisible(analog, true);
     m_interfaceItems.add(analog);
 
     // --- S88 module amount ---
-    Attributes::addCategory(s88amount, "Märklin 6050");
-    Attributes::addDisplayName(s88amount, "s88 module amount");
+    Attributes::addCategory(s88amount, "category:marklin_6050");
+    Attributes::addDisplayName(s88amount, DisplayName::Marklin6050::s88ModuleAmount);
+    Attributes::addHelp(s88amount, "marklin6050_settings:s88_module_amount.help");
     Attributes::addEnabled(s88amount, true);
     Attributes::addVisible(s88amount, true);
     Attributes::addMinMax(s88amount, 0u, 61u);
@@ -58,15 +71,16 @@ Settings::Settings(Object& _parent, std::string_view parentPropertyName)
         "50ms", "100ms", "200ms", "300ms", "400ms", "500ms", "600ms",
         "700ms", "800ms", "900ms", "1s", "1.5s", "2s", "2.5s", "3s"
     };
-    Attributes::addCategory(s88interval, "Märklin 6050");
-    Attributes::addDisplayName(s88interval, "s88 call interval");
+    Attributes::addCategory(s88interval, "category:marklin_6050");
+    Attributes::addDisplayName(s88interval, DisplayName::Marklin6050::s88CallInterval);
+    Attributes::addHelp(s88interval, "marklin6050_settings:s88_call_interval.help");
     Attributes::addEnabled(s88interval, true);
     Attributes::addVisible(s88interval, true);
     Attributes::addValues(s88interval, intervals);
     Attributes::addAliases(s88interval, &intervals, &intervalLabels);
     m_interfaceItems.add(s88interval);
 
-    // --- Turnout time ---
+    // --- Accessory OFF time ---
     static const std::vector<unsigned int> turnoutTimes = {
         25, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000
     };
@@ -74,19 +88,26 @@ Settings::Settings(Object& _parent, std::string_view parentPropertyName)
         "25ms", "50ms", "100ms", "200ms", "300ms", "400ms",
         "500ms", "600ms", "700ms", "800ms", "900ms", "1s"
     };
-    Attributes::addCategory(turnouttime, "Märklin 6050");
-    Attributes::addDisplayName(turnouttime, "Accessory OFF time");
+    Attributes::addCategory(turnouttime, "category:marklin_6050");
+    Attributes::addDisplayName(turnouttime, DisplayName::Marklin6050::accessoryOffTime);
+    Attributes::addHelp(turnouttime, "marklin6050_settings:accessory_off_time.help");
     Attributes::addEnabled(turnouttime, true);
     Attributes::addVisible(turnouttime, true);
     Attributes::addValues(turnouttime, turnoutTimes);
     Attributes::addAliases(turnouttime, &turnoutTimes, &turnoutTimeLabels);
     m_interfaceItems.add(turnouttime);
 
-    // --- Redundancy ---
-    static const std::vector<unsigned int> redundancyOptions = { 1, 2, 3, 4 };
-    static const std::vector<std::string_view> redundancyLabels = { "OFF", "2x", "3x", "4x" };
-    Attributes::addCategory(redundancy, "Märklin 6050");
-    Attributes::addDisplayName(redundancy, "Command redundancy");
+    // --- Command redundancy ---
+    static const std::vector<unsigned int> redundancyOptions = {1, 2, 3, 4};
+    static const std::vector<std::string_view> redundancyLabels = {
+        "marklin6050_settings:redundancy_off",
+        "marklin6050_settings:redundancy_2x",
+        "marklin6050_settings:redundancy_3x",
+        "marklin6050_settings:redundancy_4x"
+    };
+    Attributes::addCategory(redundancy, "category:marklin_6050");
+    Attributes::addDisplayName(redundancy, DisplayName::Marklin6050::commandRedundancy);
+    Attributes::addHelp(redundancy, "marklin6050_settings:command_redundancy.help");
     Attributes::addEnabled(redundancy, true);
     Attributes::addVisible(redundancy, true);
     Attributes::addValues(redundancy, redundancyOptions);
@@ -94,29 +115,31 @@ Settings::Settings(Object& _parent, std::string_view parentPropertyName)
     m_interfaceItems.add(redundancy);
 
     // --- Extensions ---
-    Attributes::addCategory(extensions, "Märklin 6050");
-    Attributes::addDisplayName(extensions, "Feedback Module");
+    Attributes::addCategory(extensions, "category:marklin_6050");
+    Attributes::addDisplayName(extensions, DisplayName::Marklin6050::feedbackModule);
+    Attributes::addHelp(extensions, "marklin6050_settings:feedback_module.help");
     Attributes::addEnabled(extensions, true);
     Attributes::addVisible(extensions, true);
     m_interfaceItems.add(extensions);
 
     // --- Programmer ---
-    Attributes::addCategory(oldAddress, "Programmer");
-    Attributes::addDisplayName(oldAddress, "Old loco address");
+    Attributes::addCategory(oldAddress, "category:programmer");
+    Attributes::addDisplayName(oldAddress, DisplayName::Marklin6050::oldLocoAddress);
     Attributes::addEnabled(oldAddress, false);
     Attributes::addVisible(oldAddress, true);
     Attributes::addMinMax(oldAddress, 1u, 79u);
     m_interfaceItems.add(oldAddress);
 
-    Attributes::addCategory(newAddress, "Programmer");
-    Attributes::addDisplayName(newAddress, "New loco address");
+    Attributes::addCategory(newAddress, "category:programmer");
+    Attributes::addDisplayName(newAddress, DisplayName::Marklin6050::newLocoAddress);
     Attributes::addEnabled(newAddress, false);
     Attributes::addVisible(newAddress, true);
     Attributes::addMinMax(newAddress, 1u, 79u);
     m_interfaceItems.add(newAddress);
 
-    Attributes::addCategory(programmer, "Programmer");
-    Attributes::addDisplayName(programmer, "Change address");
+    Attributes::addCategory(programmer, "category:programmer");
+    Attributes::addDisplayName(programmer, DisplayName::Marklin6050::changeAddress);
+    Attributes::addHelp(programmer, "marklin6050_settings:change_address.help");
     Attributes::addEnabled(programmer, false);
     Attributes::addVisible(programmer, true);
     m_interfaceItems.add(programmer);
@@ -148,26 +171,27 @@ void Settings::updateEnabled(bool online)
     Attributes::setEnabled(s88interval, !online);
     Attributes::setEnabled(redundancy, !online);
     Attributes::setEnabled(extensions, !online);
+
+    // programmer only when online
     Attributes::setEnabled(oldAddress, online);
     Attributes::setEnabled(newAddress, online);
     Attributes::setEnabled(programmer, online);
 
     const uint16_t ver = centralUnitVersion;
 
+    // analog only on 6027/6029
     const bool analogSupport = (ver == 6027 || ver == 6029);
     Attributes::setEnabled(analog, analogSupport && !online);
-    if (!analogSupport)
+    if(!analogSupport)
         analog.setValueInternal(false);
 
-    if (ver == 6021)
-        Attributes::setEnabled(turnouttime, false);
-    else
-        Attributes::setEnabled(turnouttime, !online);
+    // 6021 handles turnout timing internally
+    Attributes::setEnabled(turnouttime, (ver != 6021) && !online);
 }
 
 void Settings::centralUnitVersionChanged(uint16_t /*value*/)
 {
-    updateEnabled(false);  // re-evaluate what's enabled
+    updateEnabled(false);
 }
 
 } // namespace Marklin6050

040eb823dbb726df6b70590f41d9eaa9c3211c2c
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-11 00:56:01 +0100

    Update settings.cpp

--- server/src/hardware/protocol/Marklin6023Interface/settings.cpp
+++ server/src/hardware/protocol/Marklin6023Interface/settings.cpp
@@ -23,8 +23,8 @@ Settings::Settings(Object& parent, std::string_view parentPropertyName)
 {
     // --- S88 module count (max 4 for 6023/6223) ---
     Attributes::addCategory(s88amount, "category:marklin_6023");
-    Attributes::addDisplayName(s88amount, DisplayName::Marklin6023::s88ModuleAmount);
-    Attributes::addHelp(s88amount, "marklin6023_settings:s88_module_amount.help");
+    Attributes::addDisplayName(s88amount, DisplayName::Marklin6050::s88ModuleAmount);
+    Attributes::addHelp(s88amount, "marklin6050_settings:s88_module_amount.help");
     Attributes::addEnabled(s88amount, true);
     Attributes::addVisible(s88amount, true);
     Attributes::addMinMax(s88amount, 0u, 4u);
@@ -39,8 +39,8 @@ Settings::Settings(Object& parent, std::string_view parentPropertyName)
         "700ms","800ms","900ms","1s","1.5s","2s","2.5s","3s"
     };
     Attributes::addCategory(s88interval, "category:marklin_6023");
-    Attributes::addDisplayName(s88interval, DisplayName::Marklin6023::s88CallInterval);
-    Attributes::addHelp(s88interval, "marklin6023_settings:s88_call_interval.help");
+    Attributes::addDisplayName(s88interval, DisplayName::Marklin6050::s88CallInterval);
+    Attributes::addHelp(s88interval, "marklin6050_settings:s88_call_interval.help");
     Attributes::addEnabled(s88interval, true);
     Attributes::addVisible(s88interval, true);
     Attributes::addValues(s88interval, intervals);
@@ -50,11 +50,11 @@ Settings::Settings(Object& parent, std::string_view parentPropertyName)
     // --- Command redundancy ---
     static const std::vector<unsigned int> redundancyOptions = {0, 1, 2, 3};
     static const std::vector<std::string_view> redundancyLabels = {
-        "$marklin6023_settings:redundancy_off$", "x2", "x3", "x4"
+        "$marklin6050_settings:redundancy_off$", "x2", "x3", "x4"
     };
     Attributes::addCategory(redundancy, "category:marklin_6023");
-    Attributes::addDisplayName(redundancy, DisplayName::Marklin6023::commandRedundancy);
-    Attributes::addHelp(redundancy, "marklin6023_settings:command_redundancy.help");
+    Attributes::addDisplayName(redundancy, DisplayName::Marklin6050::commandRedundancy);
+    Attributes::addHelp(redundancy, "marklin6050_settings:command_redundancy.help");
     Attributes::addEnabled(redundancy, true);
     Attributes::addVisible(redundancy, true);
     Attributes::addValues(redundancy, redundancyOptions);

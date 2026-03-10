8f13dbec5bf35e89a4a045cee096be8373abf906
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-10 10:47:29 +0100

    Update settings.cpp

--- server/src/hardware/protocol/Marklin6050Interface/settings.cpp
+++ server/src/hardware/protocol/Marklin6050Interface/settings.cpp
@@ -98,7 +98,7 @@ Settings::Settings(Object& _parent, std::string_view parentPropertyName)
     m_interfaceItems.add(turnouttime);
 
     // --- Command redundancy ---
-    static const std::vector<unsigned int> redundancyOptions = {1, 2, 3, 4};
+    static const std::vector<unsigned int> redundancyOptions = {0, 1, 2, 3};
     static const std::vector<std::string_view> redundancyLabels = {
         DisplayName::Marklin6050::redundancy_off, 
         "x2",

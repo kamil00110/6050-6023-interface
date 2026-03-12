efb4c8cb869ebe06a668e0d2ed6e996d04a4c8b1
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-12 23:40:25 +0100

    Update marklin6023interface.cpp

--- server/src/hardware/interface/marklin6023interface.cpp
+++ server/src/hardware/interface/marklin6023interface.cpp
@@ -1,13 +1,24 @@
 /**
- * server/src/hardware/interface/marklin6023interface.cpp
+ * This file is part of Traintastic,
+ * see <https://github.com/traintastic/traintastic>.
  *
- * Copyright (C) 2025
+ * Copyright (C) 2026 Kamil Kasprzak
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 
 #include "marklin6023interface.hpp"
 #include "../protocol/marklin6023interface/iohandler/serialiohandler.hpp"

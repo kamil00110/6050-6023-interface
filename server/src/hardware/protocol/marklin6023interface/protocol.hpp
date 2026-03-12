78199b5a219ae7425980058b2e1753f0a68eac49
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-12 11:46:24 +0100

    Update protocol.hpp

--- server/src/hardware/protocol/marklin6023interface/protocol.hpp
+++ server/src/hardware/protocol/marklin6023interface/protocol.hpp
@@ -1,6 +1,28 @@
 /**
- * server/src/hardware/protocol/Marklin6023/protocol.hpp
+ * This file is part of Traintastic,
+ * see <https://github.com/traintastic/traintastic>.
  *
+ * Copyright (C) 2026 Kamil Kasprzak
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef TRAINTASTIC_SERVER_HARDWARE_PROTOCOL_MARKLIN6023_PROTOCOL_HPP
+#define TRAINTASTIC_SERVER_HARDWARE_PROTOCOL_MARKLIN6023_PROTOCOL_HPP
+
+/**
  * Protocol constants for the Märklin 6023/6223 ASCII serial interface.
  *
  * All commands are ASCII strings terminated with CR (\r).
@@ -15,18 +37,7 @@
  *   M <addr> R\r                – accessory red  (CU handles solenoid off)
  *   M <addr> G\r                – accessory green
  *   C <contact>\r               – query S88 contact (1-based); returns "0" or "1"
- *
- * Copyright (C) 2025
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
  */
-
-#ifndef TRAINTASTIC_SERVER_HARDWARE_PROTOCOL_MARKLIN6023_PROTOCOL_HPP
-#define TRAINTASTIC_SERVER_HARDWARE_PROTOCOL_MARKLIN6023_PROTOCOL_HPP
-
 namespace Marklin6023 {
 
 constexpr char CR = '\r';

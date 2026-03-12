15c5bd8cffcfca342a29a9bc9a324a7a21bc332f
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-12 23:31:20 +0100

    Update marklin6023interface.cpp

--- server/src/hardware/interface/marklin6023interface.cpp
+++ server/src/hardware/interface/marklin6023interface.cpp
@@ -10,8 +10,8 @@
  */
 
 #include "marklin6023interface.hpp"
-#include "../protocol/Marklin6023Interface/iohandler/serialiohandler.hpp"
-#include "../protocol/Marklin6023Interface/iohandler/simulationiohandler.hpp"
+#include "../protocol/marklin6023interface/iohandler/serialiohandler.hpp"
+#include "../protocol/marklin6023interface/iohandler/simulationiohandler.hpp"
 #include "../output/list/outputlist.hpp"
 #include "../input/list/inputlist.hpp"
 #include "../decoder/list/decoderlist.hpp"

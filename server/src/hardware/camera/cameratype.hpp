dc04d2e724cf484771c87d945bf8058e7a8fd7ac
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2026-03-26 21:54:56 +0100

    Update cameratype.hpp

--- server/src/hardware/camera/cameratype.hpp
+++ server/src/hardware/camera/cameratype.hpp
@@ -1,15 +1,16 @@
 /**
- * shared/src/traintastic/enum/cameratype.hpp
+ * server/src/hardware/camera/cameratype.hpp
  *
  * This file is part of the traintastic source code.
  *
  * Copyright (C) 2025 Reinder Feenstra
  */
-#ifndef TRAINTASTIC_SHARED_TRAINTASTIC_ENUM_CAMERATYPE_HPP
-#define TRAINTASTIC_SHARED_TRAINTASTIC_ENUM_CAMERATYPE_HPP
+#ifndef TRAINTASTIC_SERVER_HARDWARE_CAMERA_CAMERATYPE_HPP
+#define TRAINTASTIC_SERVER_HARDWARE_CAMERA_CAMERATYPE_HPP
 
 #include <cstdint>
 #include <array>
+#include "../../../shared/src/traintastic/enum/enum.hpp"
 
 enum class CameraType : uint8_t
 {
@@ -26,6 +27,24 @@ constexpr std::array<CameraType, 5> cameraTypeValues = {
   CameraType::MJPEG,
   CameraType::RTMP,
   CameraType::HLS,
+};
+
+template<>
+struct EnumName<CameraType>
+{
+  static constexpr std::string_view value = "camera_type";
+};
+
+template<>
+struct EnumValues<CameraType>
+{
+  static constexpr std::array<std::pair<std::string_view, CameraType>, 5> value = {{
+    {"local", CameraType::Local},
+    {"rtsp",  CameraType::RTSP},
+    {"mjpeg", CameraType::MJPEG},
+    {"rtmp",  CameraType::RTMP},
+    {"hls",   CameraType::HLS},
+  }};
 };
 
 #endif
